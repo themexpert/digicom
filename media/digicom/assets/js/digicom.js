ba4a5d8f07966bb21a787d20cd061ec1cbca1811
shamsbd71 <shams.bd71@gmail.com>
2015-03-26 20:42:49 +0600

    issue solved: Admin Listing page toolber menu close #17

--- media/digicom/assets/js/digicom.js
+++ media/digicom/assets/js/digicom.js
@@ -34,3 +34,39 @@ jQuery(document).ready(function() {
   jQuery("body").removeClass("sidebar-right-collapse");
  });
 });
+function toGGleToolberItems(none){
+  jQuery('#toolbar-publish,#toolbar-unpublish,#toolbar-trash').css('display',none);
+}
+jQuery(document).ready(function(){
+	
+	toGGleToolberItems('none');
+
+	jQuery("input[type='checkbox']").change(function() {
+	    if(this.checked) {
+	       toGGleToolberItems('inline-block');
+	    }else{
+	    	if(!jQuery("input[type='checkbox']").is(':checked')){
+	    		toGGleToolberItems('none');
+	    	}else{
+	    		
+				form = document.getElementById('adminForm');
+				// Toggle main toggle checkbox depending on checkbox selection
+			    var c = true, i, e;
+			    for (i = 0, n = form.elements.length; i < n; i++) {
+			        e = form.elements[i];
+			        if (e.type == 'checkbox') {
+			            if (e.name != 'checkall-toggle' && e.checked == false) {
+			                c = false;
+			                break;
+			            }
+			        }
+			    }
+			    if (c) {
+			        toGGleToolberItems('none');
+			    }
+	    	}
+	    }
+
+	});
+
+});
