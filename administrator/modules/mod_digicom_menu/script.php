3173f1425377ee3cd397cfa8313a26bc99f966c0
shamsbd71 <shams.bd71@gmail.com>
2015-03-31 20:53:56 +0600

    digicom admin menu module script added

---
+++ administrator/modules/mod_digicom_menu/script.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Installation class to perform additional changes during install/uninstall/update
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_digicom
+ * @since       3.4
+ */
+class mod_digicom_menuInstallerScript
+{
+
+	/**
+	 * Method to install the extension
+	 * $parent is the class calling this method
+	 *
+	 * @return void
+	 */
+	function install($parent) 
+	{
+		//print_r($parent);die;
+	}
+ 	
+}
