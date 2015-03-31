b41e3f9561f9e79cce5d1ffe21744f7077b5851f
shamsbd71 <shams.bd71@gmail.com>
2015-03-31 20:32:23 +0600

    create menu option moved to script

--- administrator/components/com_digicom/controller.php
+++ administrator/components/com_digicom/controller.php
@@ -32,46 +32,7 @@ class DigiComController extends JControllerLegacy
 		$view   = $this->input->get('view', 'digicom');
 		$layout = $this->input->get('layout', 'default');
 
-		$this->createDigiComMenu();
-
 		return parent::display();
 	}
 
-	/**
-	 * @TODO: get this function works on Joomla 3
-	 */
-	function createDigiComMenu(){
-
-		$db = JFactory::getDBO();
-		$sql = "SELECT COUNT(*) from #__menu_types WHERE `menutype` = 'digicom_toolber' AND `title` = 'DigiCom Toolber'";
-		$db->setQuery($sql);
-		$count = $db->loadResult();
-		if(intval($count) == 0){
-			$sql = "
-				INSERT IGNORE INTO #__menu_types(`menutype`, `title`, `description`)
-				VALUES
-					('digicom_toolber', 'DigiCom Toolber', 'DigiCom Toolber Menu')
-			";
-			$db->setQuery($sql);
-			if($db->query()){
-				$sql = "SELECT `extension_id` FROM #__extensions WHERE `name`='com_digicom' AND `element`='com_digicom'";
-				$db->setQuery($sql);
-				$db->query();
-				$componentid = intval($db->loadResult());
-				$sql = "
-					INSERT IGNORE INTO `#__menu` (`menutype`, `title`, `alias`, `note`, `path`, `link`, `type`, `published`, `parent_id`, `level`, `component_id`, `checked_out`, `checked_out_time`, `browserNav`, `access`, `img`, `template_style_id`, `params`, `lft`, `rgt`, `home`, `language`, `client_id`)
-					VALUES
-						('digicom_toolber', 'Dashboard', 'dashboard', '', 'dashboard', 'index.php?option=com_digicom&view=dashboard', 'component', 1, 1, 1, ".$componentid.", 0, '0000-00-00 00:00:00', 0, 1, '', 0, '{\"menu-anchor_title\":\"\",\"menu-anchor_css\":\"\",\"menu_image\":\"\",\"menu_text\":1,\"page_title\":\"\",\"show_page_heading\":0,\"page_heading\":\"\",\"pageclass_sfx\":\"\",\"menu-meta_description\":\"\",\"menu-meta_keywords\":\"\",\"robots\":\"\",\"secure\":0}', 289, 290, 0, '*', 0),
-						('digicom_toolber', 'Downloads', 'downloads', '', 'downloads', 'index.php?option=com_digicom&view=downloads', 'component', 1, 1, 1, ".$componentid.", 0, '0000-00-00 00:00:00', 0, 1, '', 0, '{\"menu-anchor_title\":\"\",\"menu-anchor_css\":\"\",\"menu_image\":\"\",\"menu_text\":1,\"page_title\":\"\",\"show_page_heading\":0,\"page_heading\":\"\",\"pageclass_sfx\":\"\",\"menu-meta_description\":\"\",\"menu-meta_keywords\":\"\",\"robots\":\"\",\"secure\":0}', 291, 292, 0, '*', 0),
-						('digicom_toolber', 'Orders', 'order', '', 'order', 'index.php?option=com_digicom&view=orders', 'component', 1, 1, 1, ".$componentid.", 0, '0000-00-00 00:00:00', 0, 1, '', 0, '{\"menu-anchor_title\":\"\",\"menu-anchor_css\":\"\",\"menu_image\":\"\",\"menu_text\":1,\"page_title\":\"\",\"show_page_heading\":0,\"page_heading\":\"\",\"pageclass_sfx\":\"\",\"menu-meta_description\":\"\",\"menu-meta_keywords\":\"\",\"robots\":\"\",\"secure\":0}', 293, 294, 0, '*', 0),
-						('digicom_toolber', 'Profile', 'profile', '', 'profile', 'index.php?option=com_digicom&view=profile', 'component', 1, 1, 1, ".$componentid.", 0, '0000-00-00 00:00:00', 0, 1, '', 0, '{\"menu-anchor_title\":\"\",\"menu-anchor_css\":\"\",\"menu_image\":\"\",\"menu_text\":1,\"page_title\":\"\",\"show_page_heading\":0,\"page_heading\":\"\",\"pageclass_sfx\":\"\",\"menu-meta_description\":\"\",\"menu-meta_keywords\":\"\",\"robots\":\"\",\"secure\":0}', 295, 296, 0, '*', 0)
-				";
-				$db->setQuery($sql);
-				if (!$db->query()) {
-					echo "FIX ME: admin/controller.php, line: ".__LINE__.'<br />';
-					echo $db->getErrorMsg();
-				}
-			}
-		}
-	}
 }
