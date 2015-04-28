5e2558f62a47867693cc31577ba0c9305850ec4d
shamsbd71 <shams.bd71@gmail.com>
2015-04-28 13:05:13 +0600

    price helper with validation message primary return fix

--- components/com_digicom/helpers/price.php
+++ components/com_digicom/helpers/price.php
@@ -16,8 +16,43 @@ class DigiComSiteHelperPrice {
 	* show product subscription or validity period for purchase
 	*/
 	public static function getProductValidityPeriod($item){
-		//
-		
+		//print_r($item);die;
+		if(
+			!isset( $item->price_type )
+			or 
+			( $item->price_type == 0 ) 
+		) return JText::_('COM_DIGICOM_PRODUCT_EXPIRATION_NEVER');
+
+		$expiration_type = $item->expiration_type;
+
+		switch ($expiration_type) {
+			case 'year':
+				if($item->expiration_length > 1){
+					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_YEARS');
+				}else {
+					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_YEAR');
+				}
+				break;
+			
+			case 'day':
+				if($item->expiration_length > 1){
+					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_DAYS');
+				}else {
+					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_DAY');
+				}
+				break;
+			
+			default:
+				//month
+				if($item->expiration_length > 1){
+					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_MONTHS');
+				}else {
+					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_MONTH');
+				}
+				break;
+				break;
+		}
+
 	} 
 
 }
