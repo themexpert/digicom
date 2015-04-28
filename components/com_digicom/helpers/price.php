ec6588dd410b90c9ee83b00680e5fa8e63dec2d4
shamsbd71 <shams.bd71@gmail.com>
2015-04-28 12:03:48 +0600

    price helper introduced

---
+++ components/com_digicom/helpers/price.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+
+class DigiComSiteHelperPrice {
+
+	/*
+	* $item = product object
+	* show product subscription or validity period for purchase
+	*/
+	public static function getProductValidityPeriod($item){
+		//
+		
+	} 
+
+}
