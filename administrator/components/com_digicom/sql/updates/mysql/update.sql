f765f0d279837d3f858675f0bd49dd2e20ddb7a8
shamsbd71 <shams.bd71@gmail.com>
2015-05-01 12:36:19 +0600

    database table name spelling correct

--- administrator/components/com_digicom/sql/updates/mysql/update.sql
+++ administrator/components/com_digicom/sql/updates/mysql/update.sql
@@ -3,7 +3,7 @@ ADD  `price_type` TINYINT NOT NULL DEFAULT  '3' AFTER  `price` ,
 ADD  `expiration_length` TINYINT NOT NULL AFTER  `price_type` ,
 ADD  `expiration_type` VARCHAR( 15 ) NOT NULL AFTER  `expiration_length` ;
 
-CREATE TABLE IF NOT EXISTS `#__digicom_licences` (
+CREATE TABLE IF NOT EXISTS `#__digicom_licenses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
   `productid` int(11) NOT NULL,
@@ -15,6 +15,6 @@ CREATE TABLE IF NOT EXISTS `#__digicom_licences` (
   KEY `productid` (`productid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-ALTER TABLE `#__digicom_licences`
-ADD CONSTRAINT `#__digicom_licences_ibfk_2` FOREIGN KEY (`productid`) REFERENCES `#__digicom_products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `#__digicom_licences_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `#__digicom_customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `#__digicom_licenses`
+ADD CONSTRAINT `#__digicom_licenses_ibfk_2` FOREIGN KEY (`productid`) REFERENCES `#__digicom_products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `#__digicom_licenses_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `#__digicom_customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
