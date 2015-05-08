6759e08d55e47c95e3d4452b350aa9e25c431559
shamsbd71 <shams.bd71@gmail.com>
2015-05-08 17:38:24 +0600

    dashboard view updated for new license style

--- components/com_digicom/templates/default/dashboard.php
+++ components/com_digicom/templates/default/dashboard.php
@@ -37,48 +37,33 @@ $configs = $this->configs;
 			</div>			
 		</div>
 	</div>
-	<form action="<?php echo JRoute::_('index.php?options=com_digicom&view=dashboard'); ?>" name="adminForm" method="post">
+	
+	<h3><?php echo JText::_('COM_DIGICOM_DASHBOARD_MY_ACTIVE_LICENSES');?></h3>
+	<form action="<?php echo JRoute::_('index.php?options=com_digicom&view=dashboard'); ?>" name="adminForm" method="post">	
 
-		<div class="input-append">
-			<input type="text" id="filter-search" name="filter-search" class="digi-textbox"  value="<?php echo trim(JRequest::getVar('filter-search', '')); ?>" size="30" />
-			<button type="submit" class="btn"><?php echo JText::_("COM_DIGICOM_SEARCH"); ?></button>
-			<button class="btn" onclick="document.getElementById('filter-search').value='';this.form.submit();"><i class="icon-remove"></i></button>
-		</div>			
-
-		<table class="table table-bordered table-striped">
-			<thead>
-				<tr>
-					<th><?php echo JText::_("COM_DIGICOM_PRODUCT"); ?></th>
-					<th><?php echo JText::_("COM_DIGICOM_LICENSE_ISSUE_DATE"); ?></th>
-					<th><?php echo JText::_("COM_DIGICOM_LICENSE_EXPIRE_DATE"); ?></th>
-					<th><?php echo JText::_("COM_DIGICOM_LICENSE_DAY_LEFT"); ?></th>
-				</tr>
-			</thead>
-			<tbody>
+		<div class="licenced-products tx-cards">
+		<?php if(count($this->items) > 0): ?>
 			<?php
 			$i = 0;
-			if(count($this->items) > 0){
 				foreach($this->items as $key=>$licence){
 					?>
-					<tr>
-						<td>
-							<?php echo $licence->name; ?>
-						</td>
-						<td>
+					<div class="row-fluid">
+						<div class="span6"><?php echo $licence->name; ?></div>
+						<div class="span2">
 							<?php 
 							$date = new DateTime($licence->purchase);
 							$result = $date->format('d M Y');
 							echo $result;
 							?>
-						</td>						
-						<td>
+						</div>						
+						<div class="span2">
 							<?php 
 							$date = new DateTime($licence->expires);
 							$result = $date->format('d M Y');
 							?>
 							<?php echo ($licence->expires == '0000-00-00 00:00:00' ? JText::_('COM_DIGICOM_PRODUCT_EXPIRATION_NEVER') : $result);?>
-						</td>
-						<td>
+						</div>
+						<div class="span2	">
 							<span class="label label-info"><?php echo ($licence->expires == '0000-00-00 00:00:00' ? JText::_('COM_DIGICOM_PRODUCT_VALIDITY_UNLIMITED') : $licence->dayleft .' '. JText::_('COM_DIGICOM_DAYS') ) ; ?></span>
 							<?php if($licence->expires != '0000-00-00 00:00:00'):?>
 							<a target="_blank" class="btn btn-default btn-mini pull-right hasTooltip" 
@@ -87,26 +72,18 @@ $configs = $this->configs;
 								<i class="icon-calendar"></i>
 							</a>
 							<?php endif; ?>
-						</td>
-
-					</tr>
+						</div>
+					</div>
 					<?php
 					$i++;
 				}
-			}else{ ?>
-				<tr>
-					<td colspan="5">
-						<?php echo JText::_('COM_DIGICOM_ORDERS_NO_ORDER_FOUND_NOTICE'); ?>
-					</td>
-				</tr>
-			<?php } ?>
-
-			</tbody>
-		</table>
-
-		
-
-
+			?>
+		</div
+		<?php else: ?>
+			<div class="alert alert-warning">
+				<?php echo JText::_('COM_DIGICOM_ORDERS_NO_ORDER_FOUND_NOTICE'); ?>
+			</div>
+		<?php endif;?>
 	</form>
 
 	<?php DigiComSiteHelperDigicom::loadModules('digicom_footer','xhtml'); ?>
