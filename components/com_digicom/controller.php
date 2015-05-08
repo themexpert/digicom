3541a6683602e88bb50736baa7b3c46f966a57af
shamsbd71 <shams.bd71@gmail.com>
2015-05-08 14:51:17 +0600

    rearrange the cron task

--- components/com_digicom/controller.php
+++ components/com_digicom/controller.php
@@ -8,6 +8,7 @@
  */
 
 defined('_JEXEC') or die;
+jimport('joomla.log.log');
 
 /**
  * DigiCom Front Controller
@@ -60,5 +61,35 @@ class DigiComController extends JControllerLegacy
 		}
 
 		return parent::display($cachable, $safeurlparams);
+	}
+
+	/*
+	* this function need to run through cron job
+	* url: index.php?option=com_digicom&task=cron
+	* the url for cron job	
+	*/
+	function cron()
+	{
+		try
+		{
+		    $db 	= JFactory::getDbo();
+			$query 	= $db->getQuery(true);
+			$query->update($db->quoteName('#__digicom_licensess'));
+			$query->set($db->quoteName('active') . ' = ' . $db->quote('0'));
+			$query->where('DATEDIFF('.$db->quoteName('expires').', now()) <= ' .$db->quote('-1'));		
+			//echo $query->__tostring();die;
+			$db->setQuery($query);
+			$db->query();
+
+			echo JText::_('COM_DIGICOM_LICENSE_CHECK_SUCCESSFUL');
+		}
+		catch (Exception $e)
+		{
+		    JLog::addLogger(array(), JLog::ERROR);
+		    JLog::add(JText::sprintf('COM_DIGICOM_LICENSE_CHECK_FAILED_CHECK_LOG',$e->getMessage()), JLog::ERROR, 'com_digicom');
+			echo JText::_('COM_DIGICOM_LICENSE_CHECK_FAILED');
+		}
+
+		JFactory::getApplication()->close();
 	}
 }
