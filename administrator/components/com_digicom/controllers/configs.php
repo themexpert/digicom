5a4c4759544828754a883b0bd2dbe23af556f4b9
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-05-25 19:38:30 +0600

    config xml for new order email with html & raw version file

--- administrator/components/com_digicom/controllers/configs.php
+++ administrator/components/com_digicom/controllers/configs.php
@@ -116,5 +116,79 @@ class DigiComControllerConfigs extends JControllerAdmin {
 		$view->setLayout("supportedsites");
 		$view->supportedsites();
 	}
+
+	/*
+	* copyemail template
+	* to set override file;
+	*/
+
+	function copytemplate(){
+		jimport('joomla.filesystem.folder');
+		jimport('joomla.filesystem.file');
+		$app = JFactory::getApplication();
+		$type     = $this->input->get('type','email');
+		$file     = $this->input->get('file','');
+		
+		switch ($type) {
+			case 'email':
+				//
+				$template = $this->getTemplate();
+				$client   = JApplicationHelper::getClientInfo($template->client_id);
+				$htmlPath = JPath::clean($client->path . '/templates/' . $template->template . '/html/com_digicom/emails/');
+
+				// Check Html override path folder, create if not exist
+				if (!JFolder::exists($htmlPath))
+				{
+					if (!JFolder::create($htmlPath))
+					{
+						$this->setRedirect('index.php?option=com_digicom&view=configs', JText::_('COM_DIGICOM_CONFIG_EMAIL_TEMPLATES_FOLDER_CREATE_ERROR'));
+						return false;
+					}
+				}
+
+				$originalPath = JPath::clean($client->path . '/components/com_digicom/emails/');
+				$htmlFilePath = $originalPath . $file;
+				//echo $htmlPath;die;
+				if (JFile::exists($htmlFilePath))
+				{
+					$return = JFile::copy($htmlFilePath, $htmlPath.$file, '', true);
+				}else{
+					$this->setRedirect('index.php?option=com_digicom&view=configs', JText::_('COM_DIGICOM_CONFIG_EMAIL_TEMPLATES_ORIGINAL_NOT_FOUND'));
+					return false;
+				}
+				break;
+			
+			default:
+				// we will handle only email for now
+				break;
+		}
+
+		if($return){
+			$this->setRedirect('index.php?option=com_digicom&view=configs', JText::_('COM_DIGICOM_CONFIG_EMAIL_TEMPLATES_OVERRIDE_SUCCESS'));
+		}else{
+			$this->setRedirect('index.php?option=com_digicom&view=configs', JText::_('COM_DIGICOM_CONFIG_EMAIL_TEMPLATES_OVERRIDE_FAILS'));
+		}
+	}
+
+	/*
+	* getTemplate
+	* get the site template for frontend
+	*/
+
+	public static function getTemplate(){
+		// Get the database object.
+		$db = JFactory::getDbo();
+		// Build the query.
+		$query = $db->getQuery(true)
+			->select('*')
+			->from('#__template_styles')
+			->where('client_id = ' . $db->quote(0))
+			->where('home = ' . $db->quote(1));
+
+		// Check of the editor exists.
+		$db->setQuery($query);
+		return $db->loadObject();
+
+	}
 }
 
