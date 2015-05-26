96b89b85977a0f36cd174814e1667d05d6aa9991
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-05-26 23:18:27 +0600

    email template basic file

---
+++ components/com_digicom/emails/cancel-order.raw.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<?php 
+//----------------------------------------------
+// Cancel Order Raw Template , text version
+//----------------------------------------------
+?>
