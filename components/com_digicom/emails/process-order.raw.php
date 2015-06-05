07ce96ef74528f6930c32f3bbde6116e552ede09
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-06-05 16:07:15 +0600

    progress order raw

--- components/com_digicom/emails/process-order.raw.php
+++ components/com_digicom/emails/process-order.raw.php
@@ -15,3 +15,20 @@ defined('_JEXEC') or die;
 // Processing Order Raw Template , text version
 //----------------------------------------------
 ?>
+
+= [EMAIL_HEADER] =
+
+
+Hi, [CUSTOMER_USER_NAME]
+This is an email to let you inform about your order#[ORDER_ID] is in progress at [SITENAME]. The order is as follows
+
+Order #[ORDER_ID] (<time datetime="[ORDER_DATE]">[ORDER_DATE]</time>)
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+[PRODUCTS]
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+Store address : [STORE_ADDRESS] [STORE_PHONE]
+
+[FOOTER_TEXT]
