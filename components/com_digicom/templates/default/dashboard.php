37ce5461adabf3d22017973c3cace7e71fce9699
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-02 17:08:07 +0600

    fix: empty licence dashboard breaks template

--- components/com_digicom/templates/default/dashboard.php
+++ components/com_digicom/templates/default/dashboard.php
@@ -8,13 +8,12 @@
  */
 
 defined('_JEXEC') or die;
-$app=JFactory::getApplication();
-$input = $app->input;
-$Itemid = $input->get("Itemid", 0);
-
+$app			= JFactory::getApplication();
+$input		= $app->input;
+$Itemid		= $input->get("Itemid", 0);
 $customer = $this->customer->_customer;
-$user = $this->customer->_user;
-$configs = $this->configs;
+$user			= $this->customer->_user;
+$configs 	= $this->configs;
 ?>
 
 
@@ -22,7 +21,10 @@ $configs = $this->configs;
 
 	<?php DigiComSiteHelperDigicom::loadModules('digicom_toolber'); ?>
 
-	<h2 class="digi-page-title"><?php echo JText::_("COM_DIGICOM_DASHBOARD_PAGE_TITLE"); ?></h2>
+	<h2 class="digi-page-title">
+		<?php echo JText::_("COM_DIGICOM_DASHBOARD_PAGE_TITLE"); ?>
+	</h2>
+
 	<div class="row-fluid">
 		<div class="span7">
 			<p><?php echo JText::_('COM_DIGICOM_DASHBOARD_HEADER_INTRO'); ?></p>
@@ -35,37 +37,40 @@ $configs = $this->configs;
 					<?php echo JText::_('COM_DIGICOM_EMAIL'); ?>: <?php echo $user->email; ?> <br>
 					<?php echo JText::_('COM_DIGICOM_CUSTOMER_SINCE') . $customer->registerDate; ?>
 				</p>
-			</div>			
+			</div>
 		</div>
 	</div>
-	
-	<h3><?php echo JText::_('COM_DIGICOM_DASHBOARD_MY_ACTIVE_LICENSES');?></h3>
-	<form action="<?php echo JRoute::_('index.php?options=com_digicom&view=dashboard'); ?>" name="adminForm" method="post">	
-		<table class="table table-striped table-bordered">
-			<thead>
-            	<tr>
-                  	<th width="40%"><?php echo JText::_('COM_DIGICOM_PRODUCTS'); ?></th>
-                  	<th width="20%"><?php echo JText::_('COM_DIGICOM_PRODUCTS_ISSUE_DATE'); ?></th>
-                  	<th width="20%"><?php echo JText::_('COM_DIGICOM_PRODUCTS_EXPIRE_DATE'); ?></th>
-                  	<th width="20%"><?php echo JText::_('COM_DIGICOM_PRODUCTS_VALID_FOR'); ?></th>
-            	</tr>
-          	</thead>
+
+	<h3>
+		<?php echo JText::_('COM_DIGICOM_DASHBOARD_MY_ACTIVE_LICENSES');?>
+	</h3>
+
+	<form action="<?php echo JRoute::_('index.php?options=com_digicom&view=dashboard'); ?>" name="adminForm" method="post">
 		<?php if(count($this->items) > 0): ?>
+			<table class="table table-striped table-bordered">
+				<thead>
+        	<tr>
+              	<th width="40%"><?php echo JText::_('COM_DIGICOM_PRODUCTS'); ?></th>
+              	<th width="20%"><?php echo JText::_('COM_DIGICOM_PRODUCTS_ISSUE_DATE'); ?></th>
+              	<th width="20%"><?php echo JText::_('COM_DIGICOM_PRODUCTS_EXPIRE_DATE'); ?></th>
+              	<th width="20%"><?php echo JText::_('COM_DIGICOM_PRODUCTS_VALID_FOR'); ?></th>
+        	</tr>
+      	</thead>
 			<?php
 			$i = 0;
 				foreach($this->items as $key=>$licence){
 					?>
 					<tr>
 						<td><?php echo $licence->name; ?></td>
-						<td>	
-							<?php 
+						<td>
+							<?php
 								$date = new DateTime($licence->purchase);
 								$result = $date->format('d M Y');
 								echo $result;
 							?>
 						</td>
 						<td>
-							<?php 
+							<?php
 							$date = new DateTime($licence->expires);
 							$result = $date->format('d M Y');
 							?>
@@ -74,8 +79,8 @@ $configs = $this->configs;
 						<td>
 							<?php echo ($licence->expires == '0000-00-00 00:00:00' ? JText::_('COM_DIGICOM_PRODUCT_VALIDITY_UNLIMITED') : $licence->dayleft .' '. JText::_('COM_DIGICOM_DAYS') ) ; ?>
 							<?php if($licence->expires != '0000-00-00 00:00:00'):?>
-							<a target="_blank" class="hasTooltip" 
-							   title="<?php echo JText::_('COM_DIGICOM_ADD_TO_CALENDAR');?>" 
+							<a target="_blank" class="hasTooltip"
+							   title="<?php echo JText::_('COM_DIGICOM_ADD_TO_CALENDAR');?>"
 							   href="<?php echo DigiComSiteHelperDigicom::prepareGCalendarUrl($licence);?>">
 								<i class="icon-calendar"></i>
 							</a>
