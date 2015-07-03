8fb430b661ea7ca7da5d3a0d0ade732f250070ae
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-03 11:46:36 +0600

    mod cart re arrange #97

--- modules/mod_digicom_cart/mod_digicom_cart.php
+++ modules/mod_digicom_cart/mod_digicom_cart.php
@@ -17,22 +17,16 @@ JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_digicom/models', 'Dig
 
 // Lets cache some variable
 $app				= JFactory :: getApplication();
-$input				= $app->input;
-$doc 				= JFactory::getDocument();
+$input			= $app->input;
 
-$customer	= new DigiComSiteHelperSession();
-$cart		= JModelLegacy::getInstance('Cart', 'DigiComModel', array('ignore_request' => true));
-$configs	= JComponentHelper::getComponent('com_digicom')->params;
+$customer	  = new DigiComSiteHelperSession();
+$cart		    = JModelLegacy::getInstance('Cart', 'DigiComModel', array('ignore_request' => true));
+$configs	  = JComponentHelper::getComponent('com_digicom')->params;
+$list       = $cart->getCartItems ($customer, $configs);
+$item       = $app->getMenu()->getItems('link', 'index.php?option=com_digicom&view=cart', true);
+$Itemid     = isset($item->id) ? '&Itemid=' . $item->id : '';
 
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
-$price_format = '%'.$configs->get('totaldigits').'.'.$configs->get('decimaldigits').'f';
-
-$list = $cart->getCartItems ($customer, $configs);
-
-$item = $app->getMenu()->getItems('link', 'index.php?option=com_digicom&view=cart', true);
-$Itemid = isset($item->id) ? '&Itemid=' . $item->id : '';
-
-// Load style file
-$doc->addStyleSheet( JUri::root(true). '/modules/mod_digicom_cart/assets/css/mod_digicom_cart.css');
+$moduleclass_sfx  = htmlspecialchars($params->get('moduleclass_sfx'));
+$price_format     = '%'.$configs->get('totaldigits').'.'.$configs->get('decimaldigits').'f';
 
 require JModuleHelper::getLayoutPath('mod_digicom_cart', $params->get('layout', 'default'));
