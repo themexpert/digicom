500f507b9dcc1a426b9b112570309d10c540c57c
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-06 17:13:27 +0600

    cart popup fixed to get counter changes with header. close #105

--- components/com_digicom/templates/default/cart_popup.php
+++ components/com_digicom/templates/default/cart_popup.php
@@ -42,15 +42,39 @@ $totalfields = 0;
 $shippingexists = 0;
 $from = $input->get("from", "");
 $nr_columns = 4;
-?>
-
-<?php 
-	$formlink = JRoute::_("index.php?option=com_digicom&view=cart&Itemid=".$Itemid);
-	$currency = $configs->get('currency','USD');
+$tax = $this->tax;
+$formlink = JRoute::_("index.php?option=com_digicom&view=cart&Itemid=".$Itemid);
+$currency = $configs->get('currency','USD');
 ?>
 
 <form name="cart_form" method="post" action="<?php echo $formlink?>" onSubmit="return cartformsubmit();">
 	<table class="table table-hover table-striped">
+		<thead>
+		<tr valign="top">
+			<th width="30%">
+				<?php echo JText::_("COM_DIGICOM_IMAGE");?>
+			</th>
+			<th width="30%">
+				<?php echo JText::_("COM_DIGICOM_PRODUCT");?>
+			</th>
+			<th>
+				<?php echo JText::_("COM_DIGICOM_PRICE_PLAN");?>
+			</th>
+
+			<th>
+				<?php echo JText::_("COM_DIGICOM_QUANTITY"); ?>
+			</th>
+			<?php if ($tax['discount_calculated']){?>
+			<th>
+				<?php echo JText::_("COM_DIGICOM_PROMO_DISCOUNT"); ?>
+			</th>
+			<?php } ?>
+
+			<th><?php echo JText::_("COM_DIGICOM_SUBTOTAL");?></th>
+
+			<th><?php echo JText::_("COM_DIGICOM_CART_REMOVE_ITEM");?></th>
+		</tr>
+	</thead>
 	<tbody><?php
 	$k = 0;
 	foreach($items as $itemnum => $item){
@@ -79,16 +103,28 @@ $nr_columns = 4;
 			<!-- /End Product name -->
 
 			<!-- Price -->
-			<td align="right" style="vertical-align:top;text-align:right;">
-				<?php 
+			<td align="center" style="vertical-align:top;">
+				<?php
 					echo DigiComSiteHelperDigiCom::format_price2($item->price, $item->currency, true, $configs);
 					$currency = $item->currency;
 				?>
 			</td>
 			<!-- /End Price -->
 
+			<td align="center" nowrap="nowrap">
+				<span class="digicom_details">
+					<strong> <?php echo $item->quantity; ?> </strong>
+				</span>
+			</td>
+
+			<td nowrap>
+				<span id="cart_item_total<?php echo $item->cid; ?>" class="digi_cart_amount"><?php
+					echo DigiComSiteHelperDigiCom::format_price2($item->subtotal-(isset($value_discount) ? $value_discount : 0), $item->currency, true, $configs); ?>
+				</span>
+			</td>
+
 			<!-- Remove -->
-			<td align="center" style="vertical-align:top;width:80px;text-align:right;">
+			<td align="center" style="vertical-align:top;width:80px;">
 				<a href="#" onclick="javascript:deleteFromCart(<?php echo $item->cid; ?>);"><i class="icon-remove"></i></a>
 			</td>
 			<!-- /End Remove -->
@@ -102,13 +138,13 @@ $nr_columns = 4;
 		<tfoot>
 		<tr class="info">
 			<td></td>
-			<td>
+			<td colspan="3">
 				<b><?php
 					$text = "COM_DIGICOM_ITEM_IN_CART";
 					if($k > 1){
 						$text = "COM_DIGICOM_ITEMS_IN_CART";
 					}
-					echo $k." ".JText::_($text); 
+					echo $k." ".JText::_($text);
 				?></b>
 			</td>
 			<td><b><?php echo JText::_("COM_DIGICOM_SUBTOTAL");?></b></td>
