e377f1c0ddbc832c2c27c450ce8c4654b8134568
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-08 13:48:44 +0600

    stats by users to get customers all downloads history

--- administrator/components/com_digicom/views/reports/tmpl/default_downloads.php
+++ administrator/components/com_digicom/views/reports/tmpl/default_downloads.php
@@ -12,6 +12,7 @@ defined ('_JEXEC') or die ("Go away.");
 <h3>Download Stats</h3>
 <ul class="nav nav-pills">
   <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=sales_by_date" class="current">Top Downloads</a></li>
-  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=sales_by_product" class="">Product Downloads</a></li>
+  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=sales_by_product" class="">Products Downloads</a></li>
+  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=sales_by_product" class="">Users Downloads</a></li>
   <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=sales_by_product" class="">Latest Downloads</a></li>
 </ul>
