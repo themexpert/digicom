5a6ad919738573c04cd28ca107deddff8a7b765c
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-08 14:11:54 +0600

    reports: customers  menu jtext implemented

--- administrator/components/com_digicom/views/reports/tmpl/default_customers.php
+++ administrator/components/com_digicom/views/reports/tmpl/default_customers.php
@@ -9,8 +9,9 @@
 
 defined ('_JEXEC') or die ("Go away.");
 ?>
-<h3>Customer Stats</h3>
+<h3><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_STATS');?></h3>
+
 <ul class="nav nav-pills">
-  <li><a href="index.php?option=com_digicom&view=reports&tab=customers&report=new_customers" class="current">New Customers</a></li>
-  <li><a href="index.php?option=com_digicom&view=reports&tab=customers&report=top_customers" class="">Top Customers</a></li>
+  <li><a href="index.php?option=com_digicom&view=reports&tab=customers&report=new_customers" class="current"><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_NEW_CUSTOMERS');?></a></li>
+  <li><a href="index.php?option=com_digicom&view=reports&tab=customers&report=top_customers" class=""><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_TOP_CUSTOMERS');?></a></li>
 </ul>
