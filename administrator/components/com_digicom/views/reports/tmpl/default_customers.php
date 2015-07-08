21fa7cd53d1bcc26e3c2f86aefaea3bddf3e6767
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-08 14:12:58 +0600

    reports: customers  menu jroute implemented

--- administrator/components/com_digicom/views/reports/tmpl/default_customers.php
+++ administrator/components/com_digicom/views/reports/tmpl/default_customers.php
@@ -12,6 +12,6 @@ defined ('_JEXEC') or die ("Go away.");
 <h3><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_STATS');?></h3>
 
 <ul class="nav nav-pills">
-  <li><a href="index.php?option=com_digicom&view=reports&tab=customers&report=new_customers" class="current"><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_NEW_CUSTOMERS');?></a></li>
-  <li><a href="index.php?option=com_digicom&view=reports&tab=customers&report=top_customers" class=""><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_TOP_CUSTOMERS');?></a></li>
+  <li><a href="<?php echo JRoute::_('index.php?option=com_digicom&view=reports&tab=customers&report=new_customers');?>" class="current"><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_NEW_CUSTOMERS');?></a></li>
+  <li><a href="<?php echo JRoute::_('index.php?option=com_digicom&view=reports&tab=customers&report=top_customers');?>" class=""><?php echo JText::_('COM_DIGICOM_REPORTS_CUSTOMERS_TOP_CUSTOMERS');?></a></li>
 </ul>
