aa9b599fedd0e407513a2385d4ea2028d7f51f69
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-08 14:39:30 +0600

    reports: downloads menu jroute & jtext implemented

--- administrator/components/com_digicom/views/reports/tmpl/default_downloads.php
+++ administrator/components/com_digicom/views/reports/tmpl/default_downloads.php
@@ -9,10 +9,11 @@
 
 defined ('_JEXEC') or die ("Go away.");
 ?>
-<h3>Download Stats</h3>
+<h3><?php echo JText::_('COM_DIGICOM_REPORTS_DOWNLOADS_STATS');?></h3>
+
 <ul class="nav nav-pills">
-  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=top_downloads" class="current">Top Downloads</a></li>
-  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=products_downloads" class="">Products Downloads</a></li>
-  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=users_downloads" class="">Users Downloads</a></li>
-  <li><a href="index.php?option=com_digicom&view=reports&tab=downloads&report=latest_downloads" class="">Latest Downloads</a></li>
+  <li><a href="<?php echo JRoute::_('index.php?option=com_digicom&view=reports&tab=downloads&report=top_downloads');?>" class="current"><?php echo JText::_('COM_DIGICOM_REPORTS_DOWNLOADS_TOP_DOWNLOADS');?></a></li>
+  <li><a href="<?php echo JRoute::_('index.php?option=com_digicom&view=reports&tab=downloads&report=products_downloads');?>" class=""><?php echo JText::_('COM_DIGICOM_REPORTS_DOWNLOADS_PRODUCTS_DOWNLOADS');?></a></li>
+  <li><a href="<?php echo JRoute::_('index.php?option=com_digicom&view=reports&tab=downloads&report=users_downloads');?>" class=""><?php echo JText::_('COM_DIGICOM_REPORTS_DOWNLOADS_USERS_DOWNLOADS');?></a></li>
+  <li><a href="<?php echo JRoute::_('index.php?option=com_digicom&view=reports&tab=downloads&report=latest_downloads');?>" class=""><?php echo JText::_('COM_DIGICOM_REPORTS_DOWNLOADS_LATEST_DOWNLOADS');?></a></li>
 </ul>
