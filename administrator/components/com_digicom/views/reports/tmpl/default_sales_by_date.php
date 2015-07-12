7a8168b127bfd88ad786e319665364dacdf9d3a5
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-12 11:20:06 +0600

    sample presentation

--- administrator/components/com_digicom/views/reports/tmpl/default_sales_by_date.php
+++ administrator/components/com_digicom/views/reports/tmpl/default_sales_by_date.php
@@ -9,3 +9,17 @@
 
 defined ('_JEXEC') or die ("Go away.");
 ?>
+
+<div id="chart"></div>
+
+<script type="text/javascript">
+var chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    columns: [
+      ['Money', 30, 200, 100, 400, 150, 250],
+      ['Sales', 50, 20, 10, 40, 15, 25]
+    ]
+  }
+});
+</script>
