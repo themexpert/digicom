4ae743ec17b99daa14d4529319d4b0b2fbf559a7
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-13 12:20:09 +0600

    2 sidebar moved to sidebars layout folder

---
+++ administrator/components/com_digicom/layouts/sidebars/sidebar-right.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package		DigiCom
+ * @copyright	Copyright (c)2010-2015 ThemeXpert
+ * @license 	GNU General Public License version 3, or later
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+$uri = JFactory::getURI(); 
+$pageURL = $uri->toString(); 
+?>
+
+<div id="toggle_settings_wrap" class="">
+	<h3>Global Settings</h3>
+	<ul>
+		<?php if (JFactory::getUser()->authorise('core.admin', 'com_digicom')) { ?>
+			<li><a href="index.php?option=com_config&view=component&component=com_digicom&return=<?php echo base64_encode($pageURL);?>">ACL Settings</a></li>
+			<li><a href="index.php?option=com_digicom&view=configs">Settings</a></li>
+		<?php } ?>		
+	</ul>
+
+	<?php
+	$dispatcher = JDispatcher::getInstance();
+	$results = $dispatcher->trigger( 'onAfterSidebarMenu', array());
+	?>
+
+</div>
