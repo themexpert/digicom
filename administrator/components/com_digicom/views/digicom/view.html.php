55707780ec6ad805756625f31c6984ef51ca1516
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-13 12:10:11 +0600

    dashboard acl applied for toolber

--- administrator/components/com_digicom/views/digicom/view.html.php
+++ administrator/components/com_digicom/views/digicom/view.html.php
@@ -29,26 +29,26 @@ class digicomViewdigicom extends JViewLegacy {
 	 *
 	 * @return  void
 	 */
-	
-	function display ($tpl =  null ) {		
-		
+
+	function display ($tpl =  null ) {
+
 		$this->latest_orders = DigiComHelperDigiCom::getOrders(5);
 		$this->most_sold = DigiComHelperDigiCom::getMostSoldProducts(5);
-		
+
 		$this->totalOrder = $this->get('reportTotal');
 		$this->reportOrders = $this->get('reportOrders');
 		$this->reportCustomer = $this->get('reportCustomer');
 		$this->configs = $this->get('configs');
-		
+
 		//load the toolber
 		$this->addToolbar();
 
 		DigiComHelperDigiCom::addSubmenu('digicom');
-		$this->sidebar = DigiComHelperDigiCom::renderSidebar();		
-				
+		$this->sidebar = DigiComHelperDigiCom::renderSidebar();
+
 		parent::display($tpl);
 	}
-	
+
 	/**
 	 * Add the page title and toolbar.
 		*
@@ -57,22 +57,25 @@ class digicomViewdigicom extends JViewLegacy {
 	protected function addToolbar()
 	{
 		JToolBarHelper::title(JText::_('COM_DIGICOM_DASHBOARD_TOOLBAR_TITLE'), 'generic.png');
-		
+		$canDo = JHelperContent::getActions('com_digicom', 'component');
+
 		$bar = JToolBar::getInstance('toolbar');
-		
+
 		$layout = new JLayoutFile('toolbar.title');
 		$title = array(
 			'title' => JText::_( 'COM_DIGICOM_DASHBOARD_TOOLBAR_TITLE' ),
 			'class' => 'product'
 		);
-		$bar->appendButton('Custom', $layout->render($title), 'title');		
-		
-		$layout = new JLayoutFile('toolbar.products');
-		$bar->appendButton('Custom', $layout->render(array()), 'products');
-		
+		$bar->appendButton('Custom', $layout->render($title), 'title');
+
+		if ($canDo->get('core.create')){
+			$layout = new JLayoutFile('toolbar.products');
+			$bar->appendButton('Custom', $layout->render(array()), 'products');			
+		}
+
 		$layout = new JLayoutFile('toolbar.settings');
 		$bar->appendButton('Custom', $layout->render(array()), 'settings');
 
 	}
-	
+
 }
