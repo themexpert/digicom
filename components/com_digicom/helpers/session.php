e3e86d6ae0c301bed1164509010d196a5cdd7a87
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-14 15:32:31 +0600

    session helper updated

--- components/com_digicom/helpers/session.php
+++ components/com_digicom/helpers/session.php
@@ -9,17 +9,35 @@
 
 defined('_JEXEC') or die;
 
-// TODO : PHP property visibility man.
+/**
+ * Digicom Component Session Helper
+ *
+ * @since  1.0.0
+ */
+class DigiComSiteHelperSession
+{
+	/*
+	* session id, int
+	*/
+	public $_sid = null;
+	/*
+	* joomla user, object
+	*/
+	public $user = null;
+	/*
+	* digicom customer, object
+	*/
+	public $_customer;
 
-class DigiComSiteHelperSession {
-	var $_sid = null;
-	var $user = null;
-	var $_customer;
-	var $_Itemid = null;
-
-	function __construct () {
-
-		global $Itemid;
+	/**
+	 * Class constructor
+	 *
+	 * @return  return customer object
+	 *
+	 * @since   1.0.0
+	 */
+	public function __construct ()
+	{
 
 		$db = JFactory::getDBO();
 		$my = JFactory::getUser();
@@ -27,7 +45,8 @@ class DigiComSiteHelperSession {
 		$time = time();
 		$digicomid = 'digicomid';
 		$sid = $reg->get($digicomid, 0);
-		
+
+		// first we will remove all session n cart info from db that passed 24 hours
 		$sql = "SELECT GROUP_CONCAT(sid) as sid from #__digicom_session where create_time<'".($time - 3600*24)."'";
 		$db->setQuery($sql);
 		$oldsids = $db->loadObject();
@@ -35,57 +54,62 @@ class DigiComSiteHelperSession {
 			$sql = "delete from #__digicom_cart where `sid` in (".$oldsids->sid.")";
 			$db->setQuery($sql);
 			$db->query();
-		
+
 			$sql = "delete from #__digicom_session where `sid` in (".$oldsids->sid.")";
 			$db->setQuery($sql);
 			$db->query();
 		}
-		
+
+		//as we already removed all 24h old sessions, we need to check if we have current one or not
 		if (!$sid) {
+			// so we dont have any digicomid, we need to create one
+			// but before that lets checck in session table if we have with user id
 			$sql = "select * from #__digicom_session where uid='".$my->id."'";
 			$db->setQuery($sql);
 			$digisession = $db->loadObject();
 			if(isset($digisession->sid)){
+				// we have session id, so update the time
 				$sql = "UPDATE #__digicom_session SET `create_time`='".time()."'";
 				$db->setQuery($sql);
 				$db->query();
 				$reg->set($digicomid, $digisession->sid);
 			}else{
+				// we dont have session id, create id
 				$sql = "INSERT INTO #__digicom_session (`uid`,`create_time`, `cart_details`, `transaction_details`, `shipping_details`)
 					VALUES
 					('".$my->id."','".$time."', '', '', '')
-					 ";
+				 ";
 				$db->setQuery($sql);
 				$db->query();
 				$sid = $db->insertId();
 				$reg->set($digicomid, $sid);
 			}
 		} else {
-			//check if has userid
-			if($my->id != 0){ //i'm loged in
+			// we have sessionid  $sid
+			// check if has userid
+			if($my->id != 0){
+				//i'm loged in
 				$sql = "select * from #__digicom_session where sid='".$sid."'";
 				$db->setQuery($sql);
 				$digisession = $db->loadObject();
-				
-				
-				
+
 				if($digisession->uid == 0){
-					
-					$sql = "delete from #__digicom_session where uid='".$my->id."'";
-					$db->setQuery($sql);
-					$db->query();
-					
+					//no userid
 					$sql = "UPDATE #__digicom_session SET `uid`='".$my->id."' WHERE sid='".$sid."'";
 					$db->setQuery($sql);
 					$db->query();
+					$digisession->uid = $my->id;
 				}
-				
+
 			}else{
+				//we have userid
 				$sql = "select * from #__digicom_session where uid='".$my->id."'";
 				$db->setQuery($sql);
-				$digisession = $db->loadObject();				
+				$digisession = $db->loadObject();
+
 			}
-			
+
+			// reset time of too old
 			$sid_time = $digisession->create_time;
 			if (!$sid_time || ($sid_time + 3600*24) < $time) {
 				$sql = "delete from #__digicom_session where sid='".$sid."'";
@@ -102,30 +126,58 @@ class DigiComSiteHelperSession {
 			}
 		}
 
+		// set session id
 		$this->_sid = $sid;
-		$this->_Itemid = $Itemid;
-		//print_r($my->registerDate);die;
+		// set user object
 		$this->_user = $my;
+
+		// set the customer info
 		if ($this->_user->id > 0) {
 			$sql ="select * from #__digicom_customers where email='".$this->_user->email."'";
 			$db->setQuery($sql);
 			$tmp = $db->loadObject();
 
+			// update customer info if re-registered as customer
 			if(isset($tmp) and $tmp->id != $this->_user->id){
 				$query = "UPDATE `#__digicom_customers` SET `id`=".$this->_user->id." WHERE `email`='" . $this->_user->email."'";
 				$db->setQuery( $query );
 				$db->query();
 			}
 
-			if ( $tmp ) {
+			if ( isset($tmp) ) {
+				// as we have userlogedin, make use we fill info for customer table
 				$this->_customer = $tmp;
+
+				$name_array = explode(" ", $my->name);
+				$first_name = "";
+				$last_name = "";
+				if(count($name_array) == 1){
+					$name = $my->name;
+					$first_name = $name;
+					$last_name = $name;
+				} else {
+					$last_name = $name_array[count($name_array)-1];
+					unset($name_array[count($name_array)-1]);
+					$first_name = implode(" ", $name_array);
+				}
+
+				$email = $my->email;
+
+				if (empty( $this->_customer->firstname )&& $my->id ) $this->_customer->firstname 	= $first_name;
+				if (empty( $this->_customer->lastname )&& $my->id ) $this->_customer->lastname 	= $last_name;
+				if (empty( $this->_customer->email )&& $my->id ) $this->_customer->email = $email;
+
 			} else {
+				//user but not customer
 				$this->_customer = new stdClass();
 			}
+
 		} else {
+			// guest access
 			$this->_customer = new stdClass();
 		}
-		
+
+		// dont allow empty value, so define blank
 		if (!isset($this->_customer->firstname)) $this->_customer->firstname = '';
 		if (!isset($this->_customer->lastname)) $this->_customer->lastname = '';
 		if (!isset($this->_customer->email)) $this->_customer->email = '';
@@ -133,34 +185,21 @@ class DigiComSiteHelperSession {
 		if (!isset($this->_customer->state)) $this->_customer->state = '';
 		if (!isset($this->_customer->zipcode)) $this->_customer->zipcode = '';
 		if (!isset($this->_customer->registerDate)) $this->_customer->registerDate = $my->registerDate;
-		
 		if (!isset($this->_customer->id) && $my->id ) $this->_customer->id = $my->id;
-		
-		if($my->id > 0){
-			$name_array = explode(" ", $my->name);
-			$first_name = "";
-			$last_name = "";
-			if(count($name_array) == 1){
-				$name = $my->name;
-				$first_name = $name;
-				$last_name = $name;
-			} else {
-				$last_name = $name_array[count($name_array)-1];
-				unset($name_array[count($name_array)-1]);
-				$first_name = implode(" ", $name_array);
-			}
-			$email = $my->email;
-			
-			if (empty( $this->_customer->firstname )&& $my->id ) $this->_customer->firstname 	= $first_name;
-			if (empty( $this->_customer->lastname )&& $my->id ) $this->_customer->lastname 	= $last_name;
-			if (empty( $this->_customer->email )&& $my->id ) $this->_customer->email = $email;
-		
-		}
-		
+
 		return true;
 	}
 
-	function getTransactionData() {
+	/**
+	* get transaction data
+	*
+	* @return  data object for transsection from session
+	*
+	* @since   1.0.0
+	*/
+	function getTransactionData()
+	{
+		// return null of no session id $_sid
 		if (empty($this->_sid) || $this->_sid < 1) return null;
 		$db = JFactory::getDBO();
 		$sql = "select transaction_details from #__digicom_session where sid=".$this->_sid;
@@ -168,10 +207,13 @@ class DigiComSiteHelperSession {
 		$data = $db->loadResult();
 		$data = unserialize(base64_decode($data));
 
+		//if no orderid, its value less, set default and return
 		if (is_object($data) || !isset($data['cart']['orderid']) || empty($data['cart']['orderid'])){
 			$data = array();
 			$data['cart']['orderid'] = -1;
 		}
 		return $data;
+
 	}
+
 }
