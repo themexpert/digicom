90fde69741d182cfb7deed0c1e972bcbed7d4836
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-24 12:23:14 +0600

    checkout view html updated for not to use negetive index

--- components/com_digicom/views/checkout/view.html.php
+++ components/com_digicom/views/checkout/view.html.php
@@ -15,18 +15,18 @@ class DigiComViewCheckout extends JViewLegacy
 	function display($tpl = null)
 	{
 
-		$app = JFactory::getApplication();
-		$input = $app->input;
-		$Itemid = $input->get("Itemid", 0);
-		$return = base64_encode( JURI::getInstance()->toString() );
-		$customer 	= new DigiComSiteHelperSession();
+		$app 			= JFactory::getApplication();
+		$input 		= $app->input;
+		$Itemid 	= $input->get("Itemid", 0);
+		$return 	= base64_encode( JURI::getInstance()->toString() );
+		$customer = new DigiComSiteHelperSession();
 		if($customer->_user->id < 1)
 		{
 			$app->Redirect(JRoute::_('index.php?option=com_users&view=login&return='.$return.'&Itemid='.$Itemid, false));
 			return true;
 		}
 
-		$session 	= JFactory::getSession();
+		$session 		= JFactory::getSession();
 		$processor	= JRequest::getVar("processor", "");
 
 		if(empty($processor)){
@@ -34,39 +34,24 @@ class DigiComViewCheckout extends JViewLegacy
 		}else{
 			$pg_plugin 	= $processor;
 		}
-		$Itemid 	= JRequest::getInt("Itemid", "0");
+		$Itemid 		= JRequest::getInt("Itemid", "0");
 		$order_id 	= JRequest::getInt("order_id", "0");
 		$dispatcher = JDispatcher::getInstance();
-		$plugin = JPluginHelper::importPlugin( 'digicom_pay', $pg_plugin );
-
+		$plugin 		= JPluginHelper::importPlugin( 'digicom_pay', $pg_plugin );
 
 		$configs 	= JComponentHelper::getComponent('com_digicom')->params;
-		$order 		=$this->get('Order');
-		//print_r($order);die;
-		$params 	= json_decode($order->params,true);
+		$order 		= $this->get('Order');//print_r($order);die;
+		$params 	= json_decode($order->params,true);print_r($params);die;
+		$items 		= $params['products'];//print_r($items);die;
 
-		$items 		= $params['products'];
+		$vars 						= new stdClass();
+		$vars->items 			= $items;
+		$vars->order_id 	= $params['order_id'];
+		$vars->user_id 		= JFactory::getUser()->id;
+		$vars->customer		= $customer->_customer;
+		$vars->item_name 	= '';
 
-		/*
-		 * $items
-		 * [0]
-		 *	->name
-		 *	->discount
-		 *	->quantity
-		 *	->price
-		 *	->promo
-		 *	  amount = price - promo
-		 */
-		$vars = new stdClass();
-
-
-		$vars->items = $items;
-		$vars->order_id = $params['order_id'];
-		$vars->user_id = JFactory::getUser()->id;
-		$vars->customer = $customer->_customer;
-		$vars->item_name = '';
-
-		for($i=0; $i<count($items)-2; $i++)
+		for($i=0; $i<count($items); $i++)
 		{
 			$vars->item_name.= $items[$i]['name'] . ', ';
 		}
@@ -75,7 +60,8 @@ class DigiComViewCheckout extends JViewLegacy
 		$vars->cancel_return = JRoute::_(JURI::root()."index.php?option=com_digicom&Itemid=".$Itemid."&task=cart.cancel&processor={$pg_plugin}", true, 0);
 		$vars->return = $vars->url = $vars->notify_url = JRoute::_(JURI::root()."index.php?option=com_digicom&task=cart.processPayment&processor={$pg_plugin}&order_id=".$params['order_id']."&sid=".$customer->_sid, true, false);
 		$vars->currency_code = $configs->get('currency','USD');
-		$vars->amount = $items[-2]['taxed'];//+$items[-2]['shipping'];
+		//$vars->amount = $items[-2]['taxed'];//+$items[-2]['shipping'];
+		$vars->amount = $params['order_amount'];
 
 		// Triggre plugin event
 		JPluginHelper::importPlugin('digicom_pay', $pg_plugin);
