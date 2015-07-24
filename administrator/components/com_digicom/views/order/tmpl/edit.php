0ce5bb3e1926e8489e54ab3d99a5fbfab9625371
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-24 18:26:55 +0600

    admin order edit re-arrange,#125

--- administrator/components/com_digicom/views/order/tmpl/edit.php
+++ administrator/components/com_digicom/views/order/tmpl/edit.php
@@ -37,7 +37,7 @@ $date = date( $configs->get('time_format','d M Y'), $order->order_date);
 <p class="alert alert-info">
 	<?php echo JText::sprintf('COM_DIGICOM_ORDER_DETAILS_HEADER_NOTICE',$order->id,$date,$order->status); ?>
 </p>
-	
+
 <h2><?php echo JText::_('COM_DIGICOM_ORDER_DETAILS_HEADER_TITLE'); ?></h2>
 	<table class="adminlist table table-striped">
 		<thead>
@@ -46,7 +46,7 @@ $date = date( $configs->get('time_format','d M Y'), $order->order_date);
 				<th class="sectiontableheader"  >
 					<?php echo JText::_('COM_DIGICOM_PRODUCT');?>
 				</th>
-				
+
 				<th class="sectiontableheader"  >
 					<?php echo JText::_('COM_DIGICOM_ORDER_DETAILS_TOTAL_PRODUCT');?>
 				</th>
@@ -60,7 +60,7 @@ $date = date( $configs->get('time_format','d M Y'), $order->order_date);
 
 			<tbody>
 
-			<?php 
+			<?php
 			$oll_courses_total = 0;
 			//for ($i = 0; $i < $n; $i++):
 			$i = 0;
@@ -68,11 +68,11 @@ $date = date( $configs->get('time_format','d M Y'), $order->order_date);
 				if(!isset($prod->id)) break;
 				//print_r($prod);die;
 				$id = $order->id;
-				
+
 				if (!isset($prod->currency)) {
 					$prod->currency = $configs->get('currency','USD');
 				}
-				
+
 				$licenseid = $prod->id;
 				//print_r($prod);die;
 				$refund = DigiComHelperDigiCom::getRefunds($order->id, $prod->id);
@@ -99,36 +99,25 @@ $date = date( $configs->get('time_format','d M Y'), $order->order_date);
 							echo '&nbsp;<span style="color:#ff0000;"><em>('.JText::_("LICENSE_CHARGEBACK")." - ".DigiComHelperDigiCom::format_price($chargeback, $prod->currency, true, $configs).')</em></span>';
 						} ?>
 					</td>
-					
+
 				</tr><?php
 				$k = 1 - $k;
 				$i++;
 			endforeach; ?>
 
 			<tr style="border-style:none;"><td style="border-style:none;" colspan="4"><hr /></td></tr>
-			
-			<tr>
-				<td colspan="2" width="50%"><?php echo $this->form->getLabel('status'); ?></td>
-				<td colspan="2"><?php echo $this->form->getInput('status'); ?></td>
-			</tr>
-			<tr>
-				<td colspan="2" width="50%"><?php echo $this->form->getLabel('amount_paid'); ?></td>
-				<td colspan="2"><?php echo $this->form->getInput('amount_paid'); ?></td>
-			</tr>
-
-			<tr style="border-style:none;"><td style="border-style:none;" colspan="4"><hr /></td></tr>
 
 			<tr><td colspan="2" ></td>
 				<td style="font-weight:bold"><?php echo JText::_("COM_DIGICOM_SUBTOTAL");?></td>
 				<td>
-					<?php 
+					<?php
 						echo DigiComHelperDigiCom::format_price($oll_courses_total, $order->currency, true, $configs);
 					?>
 				</td>
 			</tr>
-			
+
 			<tr><td colspan="2"></td>
-				<td style="font-weight:bold"><?php echo JText::_("COM_DIGICOM_DISCOUNT");?> <strong><?php echo $order->promocode; ?></strong></td>
+				<td><strong><?php echo JText::_("COM_DIGICOM_DISCOUNT");?></strong> (<?php echo $order->promocode; ?>)</td>
 				<td><?php echo DigiComHelperDigiCom::format_price($order->discount, $order->currency, true, $configs);?></td></tr>
 			<?php if ($refunds > 0):?>
 			<tr>
@@ -170,6 +159,18 @@ $date = date( $configs->get('time_format','d M Y'), $order->order_date);
 					?>
 				</td>
 			</tr>
+
+			<tr style="border-style:none;"><td style="border-style:none;" colspan="4"><hr /></td></tr>
+
+			<tr>
+				<td colspan="2" width="50%"><?php echo $this->form->getLabel('status'); ?></td>
+				<td colspan="2"><?php echo $this->form->getInput('status'); ?></td>
+			</tr>
+			<tr>
+				<td colspan="2" width="50%"><?php echo $this->form->getLabel('amount_paid'); ?></td>
+				<td colspan="2"><?php echo $this->form->getInput('amount_paid'); ?></td>
+			</tr>
+
 			</tbody>
 
 
