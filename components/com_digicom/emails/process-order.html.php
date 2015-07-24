89b90cb2170bebe7bbc8139a75e4142d68be78cd
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-24 12:57:05 +0600

    process order email template double comment removed

--- components/com_digicom/emails/process-order.html.php
+++ components/com_digicom/emails/process-order.html.php
@@ -16,21 +16,6 @@ defined('_JEXEC') or die;
 //----------------------------------------------
 ?>
 
-<?php
-/**
- * @package     DigiCom
- * @author      ThemeXpert http://www.themexpert.com
- * @copyright   Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
- * @license     GNU General Public License version 3 or later; see LICENSE.txt
- * @since       1.0.0
- */
-
-defined('_JEXEC') or die;
-
-//----------------------------------------------
-// New Order Html Template 
-//----------------------------------------------
-?>
 
 <!DOCTYPE html>
 <html>
