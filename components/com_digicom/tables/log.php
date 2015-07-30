799b8441b567a99d45788c2764f1d55debe783d4
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-07-30 16:15:12 +0600

    log helper n table introduced #122

---
+++ components/com_digicom/tables/log.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+
+class TableLog extends JTable {
+	
+	function TableCategory (&$db) {
+		parent::__construct('#__digicom_logs', 'id', $db);
+	}
+
+}
