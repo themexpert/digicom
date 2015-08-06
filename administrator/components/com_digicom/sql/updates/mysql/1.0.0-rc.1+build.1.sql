71b2406be149c840972119f3a7007b278d47c40b
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-06 20:08:46 +0600

    fix: Publish Date save issue. close #151

--- administrator/components/com_digicom/sql/updates/mysql/1.0.0-rc.1+build.1.sql
+++ administrator/components/com_digicom/sql/updates/mysql/1.0.0-rc.1+build.1.sql
@@ -5,3 +5,7 @@ ALTER TABLE  `#__digicom_orders_details` CHANGE `expires`  `update` TIMESTAMP ON
 ALTER TABLE  `#__digicom_log` ADD `callbackid` INT NULL DEFAULT NULL COMMENT  'orderid | fileid ; quickid to find items' AFTER  `callback` ;
 
 ALTER TABLE  `#__digicom_orders` ADD `update_date` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER  `comment` ;
+
+ALTER TABLE  `#__digicom_products` CHANGE  `publish_down`  `publish_down` DATETIME NOT NULL DEFAULT  '0000-00-00 00:00:00';
+
+ALTER TABLE  `#__digicom_products` CHANGE  `publish_up`  `publish_up` DATETIME NOT NULL DEFAULT  '0000-00-00 00:00:00';
