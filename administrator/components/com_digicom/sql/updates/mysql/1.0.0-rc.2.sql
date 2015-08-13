4ed5788bcbcb043316e20e70b8c0b06f528672ca
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-13 20:04:45 +0600

    promo sql update

--- administrator/components/com_digicom/sql/updates/mysql/1.0.0-rc.2.sql
+++ administrator/components/com_digicom/sql/updates/mysql/1.0.0-rc.2.sql
@@ -15,7 +15,6 @@ CHANGE amount amount DECIMAL( 13, 3 ) NOT NULL DEFAULT '0' COMMENT 'payable amou
 CHANGE discount discount DECIMAL( 13, 3 ) NOT NULL DEFAULT '0',
 CHANGE amount_paid amount_paid DECIMAL( 13, 3 ) NOT NULL DEFAULT '0';
 
-
 ALTER TABLE  `#__digicom_products` 
 CHANGE price price DECIMAL( 12, 2 ) NOT NULL ;
 
@@ -31,3 +30,6 @@ UPDATE  `#__content_types` SET table = '{"special":{"dbtable":"#__categories","k
 UPDATE  `#__content_types` SET field_mappings = '{"common":{"core_content_item_id":"id","core_title":"title","core_state":"published","core_alias":"alias","core_created_time":"created_time","core_modified_time":"modified_time","core_body":"description","core_hits":"hits","core_publish_up":"null","core_publish_down":"null","core_access":"access","core_params":"params","core_featured":"null","core_metadata":"metadata","core_language":"language","core_images":"null","core_urls":"null","core_version":"version","core_ordering":"null","core_metakey":"metakey","core_metadesc":"metadesc","core_catid":"parent_id","core_xreference":"null","asset_id":"asset_id"},"special":{"parent_id":"parent_id","lft":"lft","rgt":"rgt","level":"level","path":"path","extension":"extension","note":"note"}}' WHERE `type_alias` ='com_digicom.category';
 UPDATE  `#__content_types` SET content_history_options = '{"formFile":"administrator/components/com_digicom/models/forms/category.xml","hideFields":["asset_id","checked_out","checked_out_time","version","lft","rgt","level","path","extension"],"ignoreChanges":["modified_user_id","modified_time","checked_out","checked_out_time","version","hits","path"],"convertToInt":["publish_up","publish_down"],"displayLookup":[{"sourceColumn":"created_user_id","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},{"sourceColumn":"access","targetTable":"#__viewlevels","targetColumn":"id","displayColumn":"title"},{"sourceColumn":"modified_user_id","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},{"sourceColumn":"parent_id","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"}]}' WHERE `type_alias` ='com_digicom.category';
 
+ALTER TABLE  `#__digicom_promocodes` 
+CHANGE  `codestart`  `codestart` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+CHANGE  `codeend`  `codeend` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
