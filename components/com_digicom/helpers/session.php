3c18de613e564dd3a6282c7734e08032af3e461b
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-14 14:16:18 +0600

    session class update

--- components/com_digicom/helpers/session.php
+++ components/com_digicom/helpers/session.php
@@ -39,91 +39,98 @@ class DigiComSiteHelperSession
 	public function __construct ()
 	{
 
-		$db = JFactory::getDBO();
-		$my = JFactory::getUser();
-		$reg = JFactory::getSession();
-		$time = time();
-		$digicomid = 'digicomid';
-		$sid = $reg->get($digicomid, 0);
-
+		$db 		= JFactory::getDBO();
+		$my 		= JFactory::getUser();
+		$reg 		= JFactory::getSession();
+		$time 		= time();
+		$digicomid 	= 'digicomid';
+		$sid 		= $reg->get($digicomid, 0);
+		//SELECT GROUP_CONCAT(sid) as sid from `fs62c_digicom_session` where `create_time` < NOW() - INTERVAL 1 DAY
+		
 		// first we will remove all session n cart info from db that passed 24 hours
-		$sql = "SELECT GROUP_CONCAT(sid) as sid from #__digicom_session where create_time<'".($time - 3600*24)."'";
+		$sql = "SELECT GROUP_CONCAT(sid) as sid from #__digicom_session where create_time< now() - INTERVAL 7 DAY";
 		$db->setQuery($sql);
 		$oldsids = $db->loadObject();
+		//echo $oldsids->sid;die;
+		
 		if(!empty($oldsids->sid)){
 			$sql = "delete from #__digicom_cart where `sid` in (".$oldsids->sid.")";
 			$db->setQuery($sql);
-			$db->query();
+			$db->execute();
 
 			$sql = "delete from #__digicom_session where `sid` in (".$oldsids->sid.")";
 			$db->setQuery($sql);
-			$db->query();
+			$db->execute();
 		}
-
+		
 		//as we already removed all 24h old sessions, we need to check if we have current one or not
 		if (!$sid) {
 			// so we dont have any digicomid, we need to create one
 			// but before that lets checck in session table if we have with user id
-			$sql = "select * from #__digicom_session where uid='".$my->id."'";
-			$db->setQuery($sql);
-			$digisession = $db->loadObject();
-			if(isset($digisession->sid)){
-				// we have session id, so update the time
-				$sql = "UPDATE #__digicom_session SET `create_time`='".time()."'";
-				$db->setQuery($sql);
-				$db->query();
-				$reg->set($digicomid, $digisession->sid);
-			}else{
-				// we dont have session id, create id
+			
+			if(!$my->id){
+				// we dont have session id, userid
 				$sql = "INSERT INTO #__digicom_session (`uid`,`create_time`, `cart_details`, `transaction_details`, `shipping_details`)
 					VALUES
-					('".$my->id."','".$time."', '', '', '')
+					('".$my->id."',now(), '', '', '')
 				 ";
+
 				$db->setQuery($sql);
-				$db->query();
+				$db->execute();
 				$sid = $db->insertId();
 				$reg->set($digicomid, $sid);
+				
 			}
-		} else {
-			// we have sessionid  $sid
-			// check if has userid
-			if($my->id != 0){
-				//i'm loged in
-				$sql = "select * from #__digicom_session where sid='".$sid."'";
-				$db->setQuery($sql);
-				$digisession = $db->loadObject();
-
-				if($digisession->uid == 0){
-					//no userid
-					$sql = "UPDATE #__digicom_session SET `uid`='".$my->id."' WHERE sid='".$sid."'";
-					$db->setQuery($sql);
-					$db->query();
-					$digisession->uid = $my->id;
-				}
-
-			}else{
-				//we have userid
+			else{
+				//we dont have session id but have userid
 				$sql = "select * from #__digicom_session where uid='".$my->id."'";
 				$db->setQuery($sql);
 				$digisession = $db->loadObject();
 
+				if($digisession->uid != 0){
+					// user have previous session id
+					$sid = $digisession->sid;
+					$reg->set($digicomid, $sid);
+
+					// we have session id, so update the time
+					$sql = "UPDATE #__digicom_session SET `create_time`= now()";
+					$db->setQuery($sql);
+					$db->execute();
+				}else{
+
+					//user dosent have anyting before pending
+					$sql = "INSERT INTO #__digicom_session (`uid`,`create_time`, `cart_details`, `transaction_details`, `shipping_details`)
+						VALUES
+						('".$my->id."',now(), '', '', '')
+					 ";
+
+					$db->setQuery($sql);
+					$db->execute();
+					$sid = $db->insertId();
+					$reg->set($digicomid, $sid);
+
+				}
+
+
 			}
 
-			// reset time of too old
-			$sid_time = $digisession->create_time;
-			if (!$sid_time || ($sid_time + 3600*24) < $time) {
-				$sql = "delete from #__digicom_session where sid='".$sid."'";
+		
+		} elseif($my->id != 0) {
+			// we have sessionid  $sid
+			// check if has userid
+			//i'm loged in with session id
+			$sql = "select * from #__digicom_session where sid='".$sid."'";
+			$db->setQuery($sql);
+			$digisession = $db->loadObject();
+
+			if($digisession->uid == 0){
+				//no userid
+				$sql = "UPDATE #__digicom_session SET `uid`='".$my->id."' WHERE sid='".$sid."'";
 				$db->setQuery($sql);
-				$db->query();
-				$sql = "INSERT INTO #__digicom_session (`uid`,`create_time`, `cart_details`, `transaction_details`, `shipping_details`)
-					VALUES
-					('".$my->id."','".$time."', '', '', '')
-					 ";
-				$db->setQuery($sql);
-				$db->query();
-				$sid = $db->insertId();
-				$reg->set($digicomid, $sid);
+				$db->execute();
+				$digisession->uid = $my->id;
 			}
+
 		}
 
 		// set session id
@@ -141,30 +148,15 @@ class DigiComSiteHelperSession
 			if(isset($tmp) and $tmp->id != $this->_user->id){
 				$query = "UPDATE `#__digicom_customers` SET `id`=".$this->_user->id." WHERE `email`='" . $this->_user->email."'";
 				$db->setQuery( $query );
-				$db->query();
+				$db->execute();
 			}
 
 			if ( isset($tmp) ) {
 				// as we have userlogedin, make use we fill info for customer table
 				$this->_customer = $tmp;
 
-				$name_array = explode(" ", $my->name);
-				$first_name = "";
-				$last_name = "";
-				if(count($name_array) == 1){
-					$name = $my->name;
-					$first_name = $name;
-					$last_name = $name;
-				} else {
-					$last_name = $name_array[count($name_array)-1];
-					unset($name_array[count($name_array)-1]);
-					$first_name = implode(" ", $name_array);
-				}
-
-				$email = $my->email;
-
-				if (empty( $this->_customer->name )&& $my->id ) $this->_customer->name 	= $first_name;
-				if (empty( $this->_customer->email )&& $my->id ) $this->_customer->email = $email;
+				if (empty( $this->_customer->name )&& $my->id ) $this->_customer->name 	= $my->name;
+				if (empty( $this->_customer->email )&& $my->id ) $this->_customer->email = $my->email;
 
 			} else {
 				//user but not customer
