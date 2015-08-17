108df6f5ea3fc028da5909252311740ab1b6a288
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-17 13:12:03 +0600

    sql update #191

--- administrator/components/com_digicom/sql/updates/mysql/1.0.0-rc.2.sql
+++ administrator/components/com_digicom/sql/updates/mysql/1.0.0-rc.2.sql
@@ -1,27 +1,27 @@
 # digicom rc2 release sql update
 ALTER TABLE  `#__digicom_orders_details` 
-CHANGE update update DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-CHANGE price price DECIMAL( 13,3 ) NOT NULL ,
-CHANGE amount_paid amount_paid DECIMAL( 13,3 ) NOT NULL;
+CHANGE `update` `update` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+CHANGE `price` `price` DECIMAL( 13,3 ) NOT NULL ,
+CHANGE `amount_paid` `amount_paid` DECIMAL( 13,3 ) NOT NULL;
 
 ALTER TABLE  `#__digicom_log`
-ADD PRIMARY KEY(id),
-CHANGE id id INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ADD PRIMARY KEY(`id`),
+CHANGE `id` `id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
 
 ALTER TABLE  `#__digicom_orders` 
-CHANGE order_date order_date DATETIME NOT NULL ,
-CHANGE price price DECIMAL( 13, 3 ) NOT NULL DEFAULT '0' COMMENT 'original price without discount',
-CHANGE amount amount DECIMAL( 13, 3 ) NOT NULL DEFAULT '0' COMMENT 'payable amount',
-CHANGE discount discount DECIMAL( 13, 3 ) NOT NULL DEFAULT '0',
-CHANGE amount_paid amount_paid DECIMAL( 13, 3 ) NOT NULL DEFAULT '0';
+CHANGE `order_date` `order_date` DATETIME NOT NULL ,
+CHANGE `price` `price` DECIMAL( 13, 3 ) NOT NULL DEFAULT '0' COMMENT 'original price without discount',
+CHANGE `amount` `amount` DECIMAL( 13, 3 ) NOT NULL DEFAULT '0' COMMENT 'payable amount',
+CHANGE `discount` `discount` DECIMAL( 13, 3 ) NOT NULL DEFAULT '0',
+CHANGE `amount_paid` `amount_paid` DECIMAL( 13, 3 ) NOT NULL DEFAULT '0';
 
 ALTER TABLE  `#__digicom_products` 
-CHANGE price price DECIMAL( 12, 2 ) NOT NULL ;
+CHANGE `price` `price` DECIMAL( 12, 2 ) NOT NULL ;
 
-ALTER TABLE #__digicom_customers 
-ADD name VARCHAR( 255 ) NOT NULL AFTER id,
-DROP firstname ,
-DROP lastname ;
+ALTER TABLE `#__digicom_customers`
+ADD `name` VARCHAR( 255 ) NOT NULL AFTER `id`,
+DROP `firstname`,
+DROP `lastname`;
 
 UPDATE  `#__content_types` SET `table` = '{"special":{"dbtable":"#__digicom_products","key":"id","type":"product","prefix":"Table","config":"array()"},"common":{"dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}' WHERE `type_alias` ='com_digicom.product';
 UPDATE  `#__content_types` SET `field_mappings` = '{"common":{"core_content_item_id":"id","core_title":"name","core_state":"published","core_alias":"alias","core_created_time":"created","core_modified_time":"modified","core_body":"fulltext","core_hits":"hits","core_publish_up":"publish_up","core_publish_down":"publish_down","core_access":"access","core_params":"attribs","core_featured":"featured","core_metadata":"metadata","core_language":"language","core_images":"images","core_urls":"urls","core_version":"version","core_ordering":"ordering","core_metakey":"metakey","core_metadesc":"metadesc","core_catid":"catid","core_xreference":"xreference","asset_id":"asset_id"},"special":{"imptotal":"imptotal","impmade":"impmade","clicks":"clicks","clickurl":"clickurl","custombannercode":"custombannercode","cid":"cid","purchase_type":"purchase_type","track_impressions":"track_impressions","track_clicks":"track_clicks"}}' WHERE  `type_alias` ='com_digicom.product';
