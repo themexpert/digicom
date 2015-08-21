32cc08e1477dbf24d9ff016da6a8e0d26ac968c5
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-21 18:49:22 +0600

    profile controller cleaned up n decupled from registration #212

--- components/com_digicom/controllers/profile.php
+++ components/com_digicom/controllers/profile.php
@@ -19,40 +19,10 @@ class DigiComControllerProfile extends JControllerLegacy
 	function __construct() {
 		parent::__construct();
 
-		$this->registerTask("add", "edit");
-		$this->registerTask("", "login");
-		$this->registerTask("register", "edit");
 		$this->registerTask("saveCustomer", "save");
-		$this->registerTask ("register", "loginRegister");
 		$this->_model = $this->getModel('Customer');
 	}
 
-	function checkNextAction($err) {
-
-		$return = JRequest::getVar("return", "");
-
-		if ((isset($err->message) && trim($err->message) != "") || $err === FALSE) {
-			$link = JRoute::_("index.php?option=com_digicom&view=profile&layout=register&return=".$return);
-			$this->setRedirect($link);
-			return true;
-		} else {
-
-			$cart_model = $this->getModel("Cart");
-
-			$configs = JComponentHelper::getComponent('com_digicom')->params;
-
-			$this->_customer = new DigiComSiteHelperSession();
-			$customer = $this->_customer;
-			$items = $cart_model->getCartItems($customer, $configs);
-			$tax = $cart_model->calc_price($items, $customer, $configs);
-
-			$link = JRoute::_("index.php?option=com_digicom&view=cart&layout=summary");
-
-			$this->setRedirect($link);
-
-			return true;
-		}
-	}
 
 	function logCustomerIn()
 	{
@@ -109,7 +79,7 @@ class DigiComControllerProfile extends JControllerLegacy
 		}else{
 			$return = JRoute::_('index.php?option=com_digicom&view=cart&layout=summary');
 		}
-		
+
 		if($new_customer){
 			$msg =  JText::_('COM_DIGICOM_REGISTRATION_SUCCESSFULL');
 		}else {
@@ -160,111 +130,5 @@ class DigiComControllerProfile extends JControllerLegacy
 		return true;
 	}
 
-	function save_x()
-	{
-
-		$session = JFactory::getSession();
-		$conf = $this->getModel( "Config" );
-		$configs = $conf->getConfigs();
-		$returnpage = JRequest::getVar("return", "");
-		//$session->set('processor',$processor);
-
-		//$link = $this->getLink($redirect);
-		$err = $this->_model->store($error);
-
-
-		if($returnpage == "login_register"){
-
-			if($err["err"] === FALSE){
-				$_SESSION["login_register_invalid"] = "notok";
-
-				$msg = JText::_("DIGI_REGISTRATION_INVALID");
-
-				$error = $err["error"];
-				if(strpos($error, 'email') !== FALSE){
-					$msg = JText::_("DIGI_REGISTRATION_INVALID_EMAIL");
-				}
-				elseif(strpos($error, 'ser name') !== FALSE){
-					$msg = JText::_("DIGI_REGISTRATION_INVALID_USERNAME");
-				}
-
-				$name = JRequest::getVar("name", "");;
-				$company = JRequest::getVar("company", "");
-				$email = JRequest::getVar("email", "");
-				$username = JRequest::getVar("username", "");
-				$password = JRequest::getVar("password", "");
-				$password_confirm = JRequest::getVar("password_confirm", "");
-				$address = JRequest::getVar("address", "");
-				$city = JRequest::getVar("city", "");
-				$zipcode = JRequest::getVar("zipcode", "");
-				$country = JRequest::getVar("country", "");
-				$state = JRequest::getVar("state", "");
-				$array = array("name"=>$name, "company"=>$company, "email"=>$email, "username"=>$username, "password"=>$password, "password_confirm"=>$password_confirm, "address"=>$address, "city"=>$city, "zipcode"=>$zipcode, "country"=>$country, "state"=>$state);
-				$_SESSION["new_customer"] = $array;
-
-				$this->setRedirect($link, $msg, "notice");
-				return false;
-			}
-			$this->checkNextAction($err["err"]);
-			return true;
-		}
-
-		if($err)
-		{
-			$msg = JText::_('DSCUSTOMERSAVED');
-		}
-		else{
-			$msg = JText::_('DSCUSTOMERSAVEERR');
-			$msg .= $error;
-			$return = JRequest::getVar("returnpage", "");
-		}
-		$link = $this->getLink();
-		$this->setRedirect($link);
-	}
-
-	function getLink($preturn = '')
-	{
-		$Itemid = JRequest::getInt('Itemid', 0);
-		$processor = JRequest::getVar("processor", "");
-		$return = JRequest::getVar("returnpage", "", "request");
-
-		if (empty($return)) {
-			$return = $preturn;
-		}
-
-		switch($return)
-		{
-			case "downloads":
-				$link = "index.php?option=com_digicom&view=downloads" . "&Itemid=" . $Itemid;
-				break;
-
-			case "checkout":
-				$link = "index.php?option=com_digicom&view=cart&layout=summary" . "&Itemid=" . $Itemid . "&processor=" . $processor;
-				break;
-
-			case "cart":
-				$link = "index.php?option=com_digicom&view=cart"."&Itemid=".$Itemid . "&processor=" . $processor;
-				break;
-
-			case "profile":
-				$link = "index.php?option=com_digicom&view=profile"."&Itemid=".$Itemid;
-				break;
-
-			case "order":
-			case "orders":
-				$link = "index.php?option=com_digicom&view=orders" . "&Itemid=" . $Itemid . "&processor=" . $processor;
-				break;
-
-			case "register":
-			case "login_register":
-				$link = "index.php?option=com_digicom&view=profile&layout=register&returnpage=register&Itemid=" . $Itemid . "&processor=" . $processor;
-				break;
-
-			default:
-				$link = "index.php?option=com_digicom&view=dashboard&Itemid=" . $Itemid;
-				break;
-		}
-		return $link;
-	}
 
 }
