6807351f8d9d2f23cee33bc46acfdd09336ae8c8
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-21 18:39:42 +0600

    Country select form field type. close #210

--- administrator/components/com_digicom/models/fields/stateoption.php
+++ administrator/components/com_digicom/models/fields/stateoption.php
@@ -17,7 +17,7 @@ defined('_JEXEC') or die;
  * @since       1.6
  */
 class JFormFieldStateOption extends JFormField {
- 
+
 	protected $type = 'StateOption';
 	/**
 	 * The initialised state of the document object.
@@ -26,9 +26,9 @@ class JFormFieldStateOption extends JFormField {
 	 * @since  1.6
 	 */
 	protected static $initialised = false;
-	
+
 	// getLabel() left out
- 
+
 	protected function getInput()
 	{
 		$html = array();
@@ -64,29 +64,58 @@ class JFormFieldStateOption extends JFormField {
 		{
 			$html[] = JHtml::_('select.genericlist', $options, $this->name, trim($attr), 'value', 'text', $value, $this->id);
 		}
-		
+
 		return implode($html);
 	}
-	
+
 	function getItems(){
-		$comparams = JComponentHelper::getComponent('com_digicom');
-		$params = $comparams->params;
-		$country = $params->get('country','United-States');
-		$db = JFactory::getDBO();
-		$query = "select state FROM #__digicom_states where country='".$country."'";
-		$db->setQuery( $query );
+		$configs = JComponentHelper::getComponent('com_digicom')->params;
+    $db 	= JFactory::getDBO();
+		$query = $db->getQuery(true)
+					->select('country')
+					->from('#__digicom_states')
+					->group('country')
+					->order('country ASC');
+		$db->setQuery($query);
 		$countries = $db->loadObjectList();
-		
-		$return = array();
-		//print_r($items);die;
-		if($this->value){
-			foreach($countries as $key=>$item){
-				$return[] = JHTML::_( 'select.option', $item->state, $item->state );
-			}
-			return $return;
+
+		$default = '';
+
+		## Initialize array to store dropdown options ##
+		$options = array();
+	    $options[] = JHTML::_('select.option', '', JText::_('COM_DIGICOM_SELECT_COUNTRY_TITLE'));
+
+		#Top Countries#
+		$topcountries = $configs->get('topcountries', array());
+
+	    $options[] = JHTML::_('select.optgroup', JText::_('COM_DIGICOM_SELECT_FAVORITE_COUNTRY_TITLE'));
+
+		if ( count( $topcountries ) > 0 ) {
+
+			foreach($topcountries as $key=>$value) :
+				## Create $value ##
+				$options[] = JHTML::_('select.option', $value, $value);
+			endforeach;
+
 		}else{
-			return $return[] = JHTML::_( 'select.option', '', JText::_('HELPERSELECTCOUNTY') );
+
+			$options[] = JHTML::_('select.option', 'United-States', 'United-States');
+			$options[] = JHTML::_('select.option', 'Canada', 'Canada');
+			$options[] = JHTML::_('select.option', 'Bangladesh', 'Bangladesh');
+
 		}
+
+
+		$options[] = JHTML::_('select.optgroup', '');
+		$options[] = JHTML::_('select.optgroup', JText::_('COM_DIGICOM_SELECT_COUNTRY_TITLE'));
+
+		foreach($countries as $key=>$value) :
+			## Create $value ##
+			$options[] = JHTML::_('select.option', $value->country, $value->country);
+		endforeach;
+
+
+		return $options;
 	}
 
 }
