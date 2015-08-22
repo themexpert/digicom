87c0caadaafc87e43508eb2c6e3fd9562084af55
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-08-22 16:00:32 +0600

    profile page html test

--- components/com_digicom/views/profile/view.html.php
+++ components/com_digicom/views/profile/view.html.php
@@ -11,67 +11,55 @@ defined('_JEXEC') or die;
 
 class DigiComViewProfile extends JViewLegacy {
 
-	function display($tpl = null)
-	{	
 
-		$customer = new DigiComSiteHelperSession();
+	protected $data;
+
+	protected $form;
+
+	protected $params;
+
+	protected $state;
+
+	public $document;
+
+	public $configs;
+
+	/**
+	 * Method to display the view.
+	 *
+	 * @param   string  $tpl  The template file to include
+	 *
+	 * @return  mixed
+	 *
+	 * @since   1.6
+	 */
+	public function display($tpl = null)
+	{
+		// Get the view data.
 		$app = JFactory::getApplication();
 		$input = $app->input;
-		$Itemid = $input->get("Itemid", 0);
-		$return = base64_encode( JURI::getInstance()->toString() );
-		if($customer->_user->id < 1)
+
+		$this->data		= $this->get('Data');
+		$this->form		= $this->get('Form');
+		$this->state	= $this->get('State');
+		$this->params	= $this->state->get('params');
+		$this->configs	=  JComponentHelper::getComponent('com_digicom')->params;
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
 		{
-			$app->Redirect(JRoute::_('index.php?option=com_users&view=login&return='.$return.'&Itemid='.$Itemid, false));
-			return true;
+			JError::raiseError(500, implode('<br />', $errors));
+
+			return false;
 		}
 
-		$db = JFactory::getDBO();
-		$configs = JComponentHelper::getComponent('com_digicom')->params;
-	
-		$this->askforbilling = $configs->get('askforbilling',1);
-		$this->askforcompany = $configs->get('askforcompany',1);
-		
-		$country_option = DigiComSiteHelperDigiCom::get_country_options($customer->_customer, false, $configs);
-		$lists['country_option'] = $country_option;
-
-		$lists['customerlocation'] = DigiComSiteHelperDigiCom::get_store_province($customer->_customer, false);
-
-		$sql = "select * from #__digicom_states where eumember='1'";
-		$db->setQuery($sql);
-		$eucs = $db->loadObjectList();
-		$eu = array();
-		foreach ($eucs as $euc) $eu[] = $euc->country ;
-
-		$this->assign("eu", $eu);
-		$eulocated = (isset($customer->country) && in_array($customer->country, $eu));
-		$this->assign("eulocated", $eulocated);
-
-
-		$cclasses = @explode("\n", $customer->taxclass);
-		$data = $this->get('listCustomerClasses');
-		$select = '<select name="taxclass" >';
-		if (count($data) > 0)
-		foreach($data as $i => $v) {
-			$select .= '<option value="'.$v->id.'" ';
-			if (in_array($v->id, $cclasses)) {
-				$select .= ' selected ' ;
-			}
-			$select .= ' > '.$v->name.'</option>';
-		}
-		$select .= '</select>';
-		$lists['customer_class'] = $select;
-
-		$this->assign("lists", $lists);
-
-		$this->assign("customer", $customer);
-		$this->assign("configs", $configs);
+		// Escape strings for HTML output
+		$this->pageclass_sfx = htmlspecialchars($this->params->get('pageclass_sfx'));
 
 		$template = new DigiComSiteHelperTemplate($this);
 		$template->rander('profile');
 
-		parent::display($tpl);
+		return parent::display($tpl);
 	}
-
-	
 
 }
