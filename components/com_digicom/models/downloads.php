79d6f1e32b88ab044e116ae25c57b498c44d3fa2
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-01 11:23:11 +0600

    download model bundle fix

--- components/com_digicom/models/downloads.php
+++ components/com_digicom/models/downloads.php
@@ -95,7 +95,7 @@ class DigiComModelDownloads extends JModelList
 			$bundleItems = array();
 			foreach($items as $key=>$product){
 				if($product->type != 'reguler'){
-					switch($product->type){
+					switch($product->bundle_source){
 						case 'category':
 							$BundleTable = JTable::getInstance('Bundle', 'Table');
 							$BundleList = $BundleTable->getFieldValues('product_id',$product->productid,$product->bundle_source);
