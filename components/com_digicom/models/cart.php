6ca3088b0c9b403705c863e60fff2e5f21a9555c
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-09 14:34:06 +0600

    model & controller update for event hook

--- components/com_digicom/models/cart.php
+++ components/com_digicom/models/cart.php
@@ -415,7 +415,7 @@ class DigiComModelCart extends JModelItem
 
 			$payprocess['discount_calculated'] = 1;
 		}
-		
+
 		$name = 'promocode'.$cust_info->_sid;
 		$justapplied = $session->get($name,false);
 
@@ -920,9 +920,10 @@ class DigiComModelCart extends JModelItem
 		$data1=array();
 		$data1['raw_data']=isset($data['raw_data'])?$data['raw_data']:array();
 		$data1['JT_CLIENT']="com_digicom";
+
 		$dispatcher=JDispatcher::getInstance();
-		JPluginHelper::importPlugin('digicom_pay',$name);
-		$dispatcher->trigger('onTP_Storelog',array($data1));
+		// JPluginHelper::importPlugin('digicom_pay',$name);
+		$dispatcher->trigger('onTP_Storelog',array($name,$data1));
 
 		return true;
 	}
