8a980d9bd47bef2f69667c377f05acfc0874da25
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-10 16:55:00 +0600

    template helper load core js n css from helper

--- components/com_digicom/helpers/template.php
+++ components/com_digicom/helpers/template.php
@@ -14,18 +14,34 @@ defined('_JEXEC') or die;
 class DigiComSiteHelperTemplate extends JViewLegacy {
 
 	protected $view = null;
+	protected $params = null;
 
 	function __construct($view)
 	{
 		// load jquery n core joomla js for language string as we require it
 		JHtml::_('jquery.framework');
 		JHtmlBehavior::core();
-		$this->addScript(JURI::root()."media/digicom/assets/js/digicom.plugin.js?site=".JURI::root());
-
 		JText::script('COM_DIGICOM_REGISTRATION_EMAIL_ALREADY_USED');
 		JText::script('COM_DIGICOM_REGISTER_USERNAME_TAKEN');
 
+		$this->params = JComponentHelper::getParams('com_digicom');
 		$this->view = $view;
+
+		// load core digicom js plugin
+		$this->addScript(JURI::root()."media/digicom/assets/js/digicom.plugin.js?site=".JURI::root());
+
+		// load core css file
+		$core_css = $this->params->get('load_core_css',true);
+		if($core_css){
+			$this->addStyleSheet(JURI::root()."media/digicom/assets/css/digicom.css");
+		}
+
+		// load bootstrap3
+		$load_bootstrap3 = $this->params->get('load_bootstrap3',true);
+		if($load_bootstrap3){
+			$this->addStyleSheet(JURI::root()."media/digicom/assets/css/digicom.css");
+		}
+
 	}
 
 	public function rander($layout = 'products', $template = null){
@@ -33,7 +49,6 @@ class DigiComSiteHelperTemplate extends JViewLegacy {
 		$this->view->setLayout($layout);
 
 		$app = JFactory::getApplication();
-		$params = JComponentHelper::getParams('com_digicom');
 		// Look for template files in component folders
 		$this->view->_addPath('template', JPATH_COMPONENT . '/templates');
 		$this->view->_addPath('template', JPATH_COMPONENT . '/templates/default');
@@ -43,10 +58,10 @@ class DigiComSiteHelperTemplate extends JViewLegacy {
 		$this->view->_addPath('template', JPATH_SITE . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates');
 
 		// Look for specific DigiCom theme files
-		if ($params->get('template','default'))
+		if ($this->params->get('template','default'))
 		{
-			$this->view->_addPath('template', JPATH_COMPONENT . '/templates/' . $params->get('template','default'));
-			$this->view->_addPath('template', JPATH_SITE . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $params->get('template','default'));
+			$this->view->_addPath('template', JPATH_COMPONENT . '/templates/' . $this->params->get('template','default'));
+			$this->view->_addPath('template', JPATH_SITE . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $this->params->get('template','default'));
 		}
 
 		if($template){
@@ -56,19 +71,17 @@ class DigiComSiteHelperTemplate extends JViewLegacy {
 
 
 		// CUSTOM CSS
-		if (is_file( JPATH_SITE . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $params->get('template','default') . '/css/style.css')) {
-			$this->addStyleSheet( JUri::root(true) . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $params->get('template','default') . '/css/style.css');
-		}elseif( is_file(JPATH_COMPONENT . '/templates/' . $params->get('template','default') . '/css/style.css') ) {
-			$this->addStyleSheet( JUri::root(true) . '/components/com_digicom/templates/' . $params->get('template','default') . '/css/style.css');
-		}else{
-			$this->addStyleSheet(JURI::root()."media/digicom/assets/css/digicom.css");
+		if (is_file( JPATH_SITE . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $this->params->get('template','default') . '/css/style.css')) {
+			$this->addStyleSheet( JUri::root(true) . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $this->params->get('template','default') . '/css/style.css');
+		}elseif( is_file(JPATH_COMPONENT . '/templates/' . $this->params->get('template','default') . '/css/style.css') ) {
+			$this->addStyleSheet( JUri::root(true) . '/components/com_digicom/templates/' . $this->params->get('template','default') . '/css/style.css');
 		}
 
 		// CUSTOM JS
-		if (is_file(JPATH_SITE .'/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $params->get('template','default') . '/js/script.js')) {
-			$this->addScript(JUri::root(true) . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $params->get('template','default') . '/js/script.js');
-		}elseif( is_file( JPATH_COMPONENT . '/templates/' . $params->get('template','default') . '/js/script.js')) {
-			$this->addScript(JUri::root(true) . '/components/com_digicom/templates/' . $params->get('template','default') . '/js/script.js');
+		if (is_file(JPATH_SITE .'/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $this->params->get('template','default') . '/js/script.js')) {
+			$this->addScript(JUri::root(true) . '/templates/' . $app->getTemplate() . '/html/com_digicom/templates/' . $this->params->get('template','default') . '/js/script.js');
+		}elseif( is_file( JPATH_COMPONENT . '/templates/' . $this->params->get('template','default') . '/js/script.js')) {
+			$this->addScript(JUri::root(true) . '/components/com_digicom/templates/' . $this->params->get('template','default') . '/js/script.js');
 		}
 
 	}
