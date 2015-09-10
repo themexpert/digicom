b0fa25500ce495c406f143ec552e7e654221c0d7
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-10 20:24:51 +0600

    paypal view update

--- plugins/digicom_pay/paypal/paypal/tmpl/default.php
+++ plugins/digicom_pay/paypal/paypal/tmpl/default.php
@@ -9,29 +9,37 @@
 
 defined('_JEXEC') or die;
 ?>
-<div class="digicom-payment-form" data-digicom-task="hide">
+<div class="digicom-payment-form">
 
-	<form data-digicom-task="formSubmit" action="<?php echo $vars->action_url ?>" class="form-horizontal autoSubmit" method="post">
-
-		<input type="hidden" name="business" value="<?php echo $vars->business ?>" />
-		<input type="hidden" name="custom" value="<?php echo $vars->order_id ?>" />
-
-		<input type="hidden" name="item_name" value="<?php echo $vars->item_name ?>" />
-		<input type="hidden" name="amount" value="<?php echo number_format( $vars->amount, 2); ?>" />
-
-		<input type="hidden" name="return" value="<?php echo $vars->return ?>" />
-		<input type="hidden" name="cancel_return" value="<?php echo $vars->cancel_return ?>" />
-		<input type="hidden" name="notify_url" value="<?php echo $vars->notify_url ?>" />
-		<input type="hidden" name="currency_code" value="<?php echo $vars->currency_code ?>" />
-		<input type="hidden" name="no_note" value="1" />
-
-		<!--//_cart when manual calc and multiple items-->
-		<input type="hidden" name="cmd" value="_xclick" />
-
-		<div class="form-actions">
-			<input type="image" name="submit" border="0"
-			src="https://www.paypal.com/en_US/i/btn/x-click-but02.gif" border="0"  value="<?php echo JText::_('SUBMIT'); ?>" alt="Make payments with PayPal - it's fast, free and secure!" />
+	<div class="container-fluid center">
+		<div class="progress progress-striped active" style="width: 50%; margin: 20px auto 40px auto;">
+			<div  id="progressBar" class="bar" style="border-radius: 3px; margin: 0; width: 100%;"></div>
 		</div>
+	</div>
 
-	</form>
+	<div data-digicom-task="hide">
+		<form data-digicom-task="formSubmit" action="<?php echo $vars->action_url ?>" class="form-horizontal autoSubmit" method="post">
+
+			<input type="hidden" name="business" value="<?php echo $vars->business ?>" />
+			<input type="hidden" name="custom" value="<?php echo $vars->order_id ?>" />
+
+			<input type="hidden" name="item_name" value="<?php echo $vars->item_name ?>" />
+			<input type="hidden" name="amount" value="<?php echo number_format( $vars->amount, 2); ?>" />
+
+			<input type="hidden" name="return" value="<?php echo $vars->return ?>" />
+			<input type="hidden" name="cancel_return" value="<?php echo $vars->cancel_return ?>" />
+			<input type="hidden" name="notify_url" value="<?php echo $vars->notify_url ?>" />
+			<input type="hidden" name="currency_code" value="<?php echo $vars->currency_code ?>" />
+			<input type="hidden" name="no_note" value="1" />
+
+			<!--//_cart when manual calc and multiple items-->
+			<input type="hidden" name="cmd" value="_xclick" />
+
+			<div class="form-actions">
+				<input type="image" name="submit" border="0"
+				src="https://www.paypal.com/en_US/i/btn/x-click-but02.gif" border="0"  value="<?php echo JText::_('SUBMIT'); ?>" alt="Make payments with PayPal - it's fast, free and secure!" />
+			</div>
+
+		</form>
+	</div>
 </div>
