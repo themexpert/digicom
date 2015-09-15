8363d4165d415bbc7b26c6b1763fe4a4f57bd9c9
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-15 14:08:47 +0600

    category item initial markup fix

--- components/com_digicom/templates/bootstrap3/category_item.php
+++ components/com_digicom/templates/bootstrap3/category_item.php
@@ -19,7 +19,7 @@ if(!isset($images->thumb_image)){
 if($this->item->price > 0){
 	$price = DigiComSiteHelperPrice::format_price($this->item->price, $this->configs->get('currency','USD'), true, $this->configs);
 }else{
-	$price = '<span class="label label-success">'.JText::_('COM_DIGICOM_PRODUCT_PRICE_FREE').'</span>';
+	$price = JText::_('COM_DIGICOM_PRODUCT_PRICE_FREE');
 }
 $link = JRoute::_(DigiComSiteHelperRoute::getProductRoute($this->item->id, $this->item->catid, $this->item->language));
 ?>
@@ -35,22 +35,23 @@ $link = JRoute::_(DigiComSiteHelperRoute::getProductRoute($this->item->id, $this
 		<?php if(!empty($this->item->bundle_source)):?>
 		<span class="bundle-label label label-warning"><?php echo JText::sprintf('COM_DIGICOM_PRODUCT_TYPE_BUNDLE');?></span>
 		<?php endif; ?>
+		<?php if($this->item->featured): ?>
+			<span class="featured label label-info">Featured</span>
+		<?php endif; ?>
 
 	<!-- Product Name & Intro text -->
 		<div class="caption">
 			<h3>
 				<a href="<?php echo $link;?>"><?php echo $this->item->name; ?></a>
-
-				<?php if($this->item->featured): ?>
-					<small class="featured label label-info">Featured</small>
-				<?php endif; ?>
 			</h3>
 			<p class="description"><?php echo $this->item->introtext; ?></p>
 
-		<!-- Price & Readmore Button -->
-			<div class="clearfix">
-				<span class="price"><?php echo $price; ?></span>
-				<a href="<?php echo $link;?>" class="btn btn-primary read-more"><?php echo JText::_('COM_DIGICOM_BUTTON_DETAILS'); ?></a>
+			<!-- Price & Readmore Button -->
+			<div class="clearfix input-group input-group-sm">
+			  <span class="input-group-addon product-price" id="product-price"><?php echo $price; ?></span>
+				<div class="input-group-btn">
+					<a href="<?php echo $link;?>" class="btn btn-primary read-more"><?php echo JText::_('COM_DIGICOM_BUTTON_DETAILS'); ?></a>
+				</div>
 			</div>
 		</div>
 	</div>
