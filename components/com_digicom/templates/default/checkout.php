b59d97cc3100827b8ef063b219809bd1f7f47be8
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-17 00:47:00 +0600

    checkout page class fix

--- components/com_digicom/templates/default/checkout.php
+++ components/com_digicom/templates/default/checkout.php
@@ -16,57 +16,65 @@ $pg_plugin = $this->pg_plugin;
 $configs = $this->configs;
 $data = $this->data;
 ?>
-<div id="digicom">
+<div id="digicom" class="dc dc-checkout">
 
 	<?php
 	$this->setLayout('cart');
 	echo $this->loadTemplate('steps');
 	?>
 
-	<h1 class="digi-page-title"><?php echo JText::sprintf("COM_DIGICOM_CHECKOUT_PAYMENT_DETAILS_PAGE_TITLE", $pg_plugin); ?></h1>
+	<h1 class="page-title"><?php echo JText::sprintf("COM_DIGICOM_CHECKOUT_PAYMENT_DETAILS_PAGE_TITLE", $pg_plugin); ?></h1>
 
-	<div class="cart-items-wrap">
+	<div class="dc-checkout-items">
+
 		<h4 class="align-center"><?php echo JText::_("COM_DIGICOM_SUMMARY_YOUR_ORDER");?></h4>
 
-		<table id="digicomcarttable" class="table table-striped table-bordered" width="100%">
-		  <thead>
-		    <tr>
-		      <th><?php echo JText::_("COM_DIGICOM_PRODUCT");?></th>
-		      <th><?php echo JText::_("COM_DIGICOM_PRICE_PLAN");?></th>
-		      <th><?php echo JText::_("COM_DIGICOM_QUANTITY"); ?></th>
-		      <th><?php echo JText::_("COM_DIGICOM_SUBTOTAL");?></th>
-		    </tr>
-		  </thead>
-		  <tbody>
-		    <?php foreach($this->items as $itemnum => $item ): ?>
-		      <tr>
-		        <td>
-							<?php echo $item->name; ?>
-		          <?php if ($this->configs->get('show_validity',1) == 1) : ?>
-		            <div class="muted">
-		              <small><?php echo JText::_('COM_DIGICOM_PRODUCT_VALIDITY'); ?> : <?php echo DigiComSiteHelperPrice::getProductValidityPeriod($item); ?></small>
-		            </div>
-		          <?php endif; ?>
-		        </td>
-		        <td><?php echo DigiComSiteHelperPrice::format_price($item->price, $item->currency, true, $this->configs); ?></td>
-		        <td><?php echo $item->quantity; ?></td>
-		        <td><?php echo DigiComSiteHelperPrice::format_price($item->subtotal-(isset($value_discount) ? $value_discount : 0), $item->currency, true, $this->configs); ?></td>
-		      </tr>
-		      <?php
-		    endforeach;
-		    ?>
-		  </tbody>
-		  <tfoot>
-				<tr>
-					<td colspan="4" style="text-align:right;">
-						Total : <strong><?php echo  DigiComSiteHelperPrice::format_price($this->order->amount, $configs->get('currency','USD'), true, $configs);?></strong>
-					</td>
-				</tr>
-		  </tfoot>
+		<div class="dc-cart-items table-responsive">
+		  <table class="dc-cart-items-table table table-striped table-bordered" width="100%">
+			  <thead>
+			    <tr>
+			      <th><?php echo JText::_("COM_DIGICOM_PRODUCT");?></th>
+			      <th><?php echo JText::_("COM_DIGICOM_PRICE_PLAN");?></th>
+			      <th><?php echo JText::_("COM_DIGICOM_QUANTITY"); ?></th>
+			      <th><?php echo JText::_("COM_DIGICOM_SUBTOTAL");?></th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <?php foreach($this->items as $itemnum => $item ): ?>
+			      <tr>
+			        <td>
+								<?php echo $item->name; ?>
+			          <?php if ($this->configs->get('show_validity',1) == 1) : ?>
+			            <div class="muted">
+			              <small><?php echo JText::_('COM_DIGICOM_PRODUCT_VALIDITY'); ?> : <?php echo DigiComSiteHelperPrice::getProductValidityPeriod($item); ?></small>
+			            </div>
+			          <?php endif; ?>
+			        </td>
+			        <td><?php echo DigiComSiteHelperPrice::format_price($item->price, $item->currency, true, $this->configs); ?></td>
+			        <td><?php echo $item->quantity; ?></td>
+			        <td><?php echo DigiComSiteHelperPrice::format_price($item->subtotal-(isset($value_discount) ? $value_discount : 0), $item->currency, true, $this->configs); ?></td>
+			      </tr>
+			      <?php
+			    endforeach;
+			    ?>
+			  </tbody>
+			  <tfoot>
+					<tr>
+						<td colspan="4" style="text-align:right;">
+							Total : <strong><?php echo  DigiComSiteHelperPrice::format_price($this->order->amount, $configs->get('currency','USD'), true, $configs);?></strong>
+						</td>
+					</tr>
+			  </tfoot>
 
-		</table>
+			</table>
+		</div>
+
+		<div class="dc-payment-output">
+			<?php echo $data[0]; ?>
+		</div>
+
 	</div>
 
-	<?php echo $data[0]; ?>
+	<?php echo DigiComSiteHelperDigiCom::powered_by(); ?>
+
 </div>
-<?php echo DigiComSiteHelperDigiCom::powered_by(); ?>
