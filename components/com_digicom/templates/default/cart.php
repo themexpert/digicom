ae889337cef8c69222c0c3f513b900f3ebed09ac
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-17 00:12:05 +0600

    cart page class fix

--- components/com_digicom/templates/default/cart.php
+++ components/com_digicom/templates/default/cart.php
@@ -9,8 +9,9 @@
 
 defined('_JEXEC') or die;
 JHtml::_('formbehavior.chosen', 'select');
+$user = JFactory::getUser();
 ?>
-<div id="digicom">
+<div id="digicom" class="dc dc-cart">
 
 	<?php if(count($this->items) == 0): ?>
 		<p class="alert alert-warning">
@@ -20,33 +21,34 @@ JHtml::_('formbehavior.chosen', 'select');
 
 		<?php echo $this->loadTemplate('steps');?>
 
-		<div class="digi-cart">
-			<?php
-			$user = JFactory::getUser();
-			if($user->id != "0"){
-			?>
-			<div class="well well-sm" style="text-align:right;vertical-align:bottom;">
-				<?php echo JText::sprintf("COM_DIGICOM_CART_LOGGED_IN_AS",$user->name); ?>
-			</div>
-			<?php } ?>
-
-
-			<form data-digicom-id="cart_form" name="cart_form" method="post" action="<?php echo JRoute::_("index.php?option=com_digicom&view=cart"); ?>">
-
-				<?php echo $this->loadTemplate('items');?>
-
-				<?php echo $this->loadTemplate('price');?>
-
-				<input name="view" type="hidden" value="cart">
-				<input name="task" type="hidden" data-digicom-id="task" value="cart.checkout">
-				<input name="returnpage" type="hidden" value="">
-
-			</form>
+		<?php if($user->id != "0"): ?>
+		<div class="well well-sm" style="text-align:right;vertical-align:bottom;">
+			<?php echo JText::sprintf("COM_DIGICOM_CART_LOGGED_IN_AS",$user->name); ?>
 		</div>
+		<?php endif; ?>
+
+		<form
+			data-digicom-id="cart_form"
+			name="dcForm"
+			id="dcForm"
+			class="form dc-cart-items"
+			method="post"
+			action="<?php echo JRoute::_("index.php?option=com_digicom&view=cart"); ?>">
+
+			<?php echo $this->loadTemplate('items');?>
+
+			<?php echo $this->loadTemplate('price');?>
+
+			<input name="option" type="hidden" value="com_digicom">
+			<input name="view" type="hidden" value="cart">
+			<input name="task" type="hidden" data-digicom-id="task" value="cart.checkout">
+
+		</form>
 
 		<?php echo $this->loadTemplate('modals');?>
 
-<?php endif; ?>
+	<?php endif; ?>
 
 	<?php echo DigiComSiteHelperDigicom::powered_by(); ?>
+
 </div>
