b7c9a0394bd80101024d36a9cb9fb53dab265e3a
Parvez Akther <parvez@themexpert.com>
2015-09-17 19:44:15 +0600

    [Refactor] Produc page (single)

--- components/com_digicom/templates/default/product.php
+++ components/com_digicom/templates/default/product.php
@@ -8,116 +8,138 @@
  */
 
 defined('_JEXEC') or die;
-$conf = $this->configs;
+$configs = $this->configs;
 $images  = json_decode($this->item->images);
+// Legacy code
+// TODO : Remove after 1.1
 if(!isset($images->image_full)){
 	$images = new stdClass();
 	$images->image_full = $this->item->images;
 }
 if($this->item->price > 0){
-	$price = '<span>'.JText::_('COM_DIGICOM_PRODUCT_PRICE').": ".DigiComSiteHelperPrice::format_price($this->item->price, $conf->get('currency','USD'), true, $conf).'</span>';
+	$price = DigiComSiteHelperPrice::format_price($this->item->price, $configs->get('currency','USD'), true, $configs).'</span>';
 }else{
 	$price = '<span>'.JText::_('COM_DIGICOM_PRODUCT_PRICE_FREE').'</span>';
 }
 $link = JRoute::_(DigiComSiteHelperRoute::getProductRoute($this->item->id, $this->item->catid, $this->item->language));
 ?>
 
-<div id="digicom" class="dc dc-product">
+<div id="digicom" class="dc dc-product" itemscope itemtype="http://schema.org/Product">
 
-	<div class="product-page<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="http://schema.org/Product">
+	<header class="dc-item-head">
+		<h1 class="dc-product-title">
+			<span itemprop="name">
+				<?php echo $this->item->name; ?>
+			</span>
+		</h1>
+	</header>
 
-		<meta itemprop="inLanguage" content="<?php echo ($this->item->language === '*') ? JFactory::getConfig()->get('language') : $this->item->language; ?>" />
-		<meta itemprop="url" content="<?php echo $link; ?>" />
-
-		<?php if ($this->params->get('show_page_heading')) : ?>
-		<div class="page-header">
-			<h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
-		</div>
-		<?php endif;?>
-		<header>
-			<h2 class="product-title">
-				<span itemprop="name">
-					<?php echo $this->item->name; ?>
-				</span>
-			</h2>
-
-			<?php if($this->item->featured):?>
-				<span class="label label-info"><?php echo JText::_('JFEATURED');?></span>
-			<?php endif; ?>
-
-			<?php if(!empty($this->item->bundle_source)):?>
-				<span class="label label-warning"><?php echo JText::sprintf('COM_DIGICOM_PRODUCT_TYPE_BUNDLE');?></span>
-			<?php endif; ?>
-
-			<?php if (!empty($this->item->tags->itemTags)) : ?>
-				<?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
-				<?php echo $this->item->tagLayout->render($this->item->tags->itemTags); ?>
-			<?php endif; ?>
-		</header>
-		<div class="product-details clearfix">
-			<?php if(!empty($images->image_full)): ?>
-				<div class="text-center">
-					<img itemprop="image" src="<?php echo JURI::root().$images->image_full; ?>" alt="<?php echo $this->item->name; ?>" class="img-responsive img-thumbnail"/>
+	<article>
+		<div class="row">
+			<div class="col-md-8">
+				<div class="dc-item-in">
+					<?php if(!empty($images->image_full)): ?>
+						<figure>
+							<img itemprop="image" src="<?php echo JURI::root().$images->image_full; ?>" alt="<?php echo $this->item->name; ?>" class="dc-product-image"/>
+						</figure>
+					<?php endif; ?>
 				</div>
-			<?php endif; ?>
-
-			<div class="description" itemprop="description">
-				<?php echo $this->item->text; ?>
+				<p class="dc-product-intro"><?php echo $this->item->introtext?></p>
+				<div class="dc-product-details" itemprop="description">
+					<?php echo $this->item->text; ?>
+				</div>
+				<?php
+				if(!empty($this->item->bundle_source)):
+					echo $this->loadTemplate('bundle');
+				endif;
+				?>
 			</div>
+			<div class="col-md-4">
+				<div class="dc-item-in">
+					<div class="well clearfix" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="dc-product-price text-right">
+									<meta itemprop="priceCurrency" content="<?php $configs->get('currency','USD');?>" />
+									<strong itemprop="price" content="<?php echo $this->item->price; ?>">
+										<?php echo $price; ?>
+									</strong>
+								</div>
+							</div>
+							<?php if ($this->configs->get('show_validity',1) == 1) : ?>
+								<div class="col-md-6">
+									<div class="dc-product-validity">
+										<small><?php echo JText::_('COM_DIGICOM_PRODUCT_VALIDITY'); ?> <br/>
+										<?php echo DigiComSiteHelperPrice::getProductValidityPeriod($this->item); ?></small>
+									</div>
+								</div>
+							<?php endif; ?>
+						</div>
+					</div>
+					<div class="dc-product-info">
+						<ul class="list-unstyled">
+							<li>
+								<strong><?php echo JText::_('COM_DIGICOM_PRODUCT_CREATE_DATE');?> :</strong> <?php echo JFactory::getDate($this->item->publish_up)->format('M d, Y')?>
+							</li>
+							<li>
+								<strong><?php echo JText::_('COM_DIGICOM_PRODUCT_CATEGORY');?>: </strong>
+								<a href="<?php echo DigiComSiteHelperRoute::getCategoryRoute($this->item->catid);?>">
+									<?php echo $this->item->category_title;?></a>
+							</li>
+							<li>
+								<strong><?php echo JText::_('COM_DIGICOM_TYPE');?> : </strong>
+								<?php if(!empty($this->item->bundle_source)):?>
+									<?php echo JText::_('COM_DIGICOM_PRODUCT_TYPE_BUNDLE');?>
+								<?php else:?>
+									<?php echo JText::_('COM_DIGICOM_PRODUCT_TYPE_SINGLE');?>
+								<?php endif; ?>
+							</li>
+							<li>
+								<strong><?php echo JText::_('COM_DIGICOM_TAGS');?> :</strong> <?php
+									if(!empty($this->item->tags->itemTags)){
+										$this->item->tagLayout = new JLayoutFile('joomla.content.tags');
+										echo $this->item->tagLayout->render($this->item->tags->itemTags);
+									}
+								?>
+							</li>
+						</ul>
+					</div>
 
-			<?php
-			if(!empty($this->item->bundle_source)):
-				echo $this->loadTemplate('bundle');
-			endif;
-			?>
-
-			<?php if ($this->configs->get('catalogue',0) == '0' and !$this->item->hide_public) : ?>
-			<div class="well clearfix" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-				<meta itemprop="priceCurrency" content="<?php $conf->get('currency','USD');?>" />
-
-				<div class="product-price pull-left">
-					<span itemprop="price" content="<?php echo $this->item->price; ?>"><?php echo $price; ?></span>
-					<br/>
-					<?php if ($this->configs->get('show_validity',1) == 1) : ?>
-					<span>
-						<small><?php echo JText::_('COM_DIGICOM_PRODUCT_VALIDITY'); ?> : <?php echo DigiComSiteHelperPrice::getProductValidityPeriod($this->item); ?></small>
-					</span>
+					<?php if($this->item->featured):?>
+						<!-- Featured label -->
+						<span class="label label-info label--featured"><?php echo JText::_('JFEATURED');?></span>
 					<?php endif; ?>
 				</div>
 
-				<div class="addtocart-bar pull-right">
-					<form name="prod" class="form-inline" id="product-form" action="<?php echo JRoute::_('index.php?option=com_digicom&view=cart');?>" method="post" style="width:100%;">
-						<div class="<?php echo ($conf->get('show_quantity',0) == 1 ? "input-group" : ''); ?>">
+				<?php if ($this->configs->get('catalogue',0) == '0' and !$this->item->hide_public) : ?>
+					<div class="dc-addtocart-bar">
+						<form name="prod" class="form-inline" id="product-form" action="<?php echo JRoute::_('index.php?option=com_digicom&view=cart');?>" method="post" style="width:100%;">
+							<div class="<?php echo ($configs->get('show_quantity',0) == 1 ? "with-qnty" : ''); ?>">
 
-							<?php if($conf->get('show_quantity',0) == "1") {	?>
-								<input data-digicom-id="quantity_<?php echo $this->item->id; ?>" type="number" name="qty" min="1" class="input-group-addon" value="1" size="2" placeholder="<?php echo JText::_('COM_DIGICOM_QUANTITY'); ?>">
-							<?php } ?>
+								<?php if($configs->get('show_quantity',0) == "1") {	?>
+									<input data-digicom-id="quantity_<?php echo $this->item->id; ?>" type="number" name="qty" min="1" class="dc-product-qnty" value="1" size="2" placeholder="<?php echo JText::_('COM_DIGICOM_QUANTITY'); ?>">
+								<?php } ?>
 
-							<div class="input-group-btn">
-								<?php if($conf->get('afteradditem',0) == "2") {	?>
-									<div type="button" class="btn btn-primary" onclick="Digicom.addtoCart(<?php echo $this->item->id; ?>,'<?php echo JRoute::_("index.php?option=com_digicom&view=cart"); ?>');"><i class="glyphicon glyphicon-cart"></i> <?php echo JText::_("COM_DIGICOM_ADD_TO_CART");?></div>
+								<?php if($configs->get('afteradditem',0) == "2") {	?>
+									<div type="button" class="btn btn-success btn-large btn-block btn--cart" onclick="Digicom.addtoCart(<?php echo $this->item->id; ?>,'<?php echo JRoute::_("index.php?option=com_digicom&view=cart"); ?>');"><?php echo JText::_("COM_DIGICOM_ADD_TO_CART");?></div>
 								<?php }else { ?>
-									<div type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-cart"></i> <?php echo JText::_('COM_DIGICOM_ADD_TO_CART'); ?></div>
+									<div type="submit" class="btn btn-success btn-large btn-block"> <?php echo JText::_('COM_DIGICOM_ADD_TO_CART'); ?></div>
 									<?php } ?>
-
 							</div>
-						</div>
 
-						<input type="hidden" name="option" value="com_digicom"/>
-						<input type="hidden" name="view" value="cart"/>
-						<input type="hidden" name="task" value="cart.add"/>
-						<input type="hidden" name="pid" value="<?php echo $this->item->id; ?>"/>
-					</form>
-				</div>
+							<input type="hidden" name="option" value="com_digicom"/>
+							<input type="hidden" name="view" value="cart"/>
+							<input type="hidden" name="task" value="cart.add"/>
+							<input type="hidden" name="pid" value="<?php echo $this->item->id; ?>"/>
+						</form>
+					</div>
+				<?php endif; ?>
 			</div>
-			<?php endif; ?>
-
 		</div>
-
-	</div>
+	</artile>
 
 	<?php
-		if($conf->get('afteradditem',0) == "2"):
+		if($configs->get('afteradditem',0) == "2"):
 			$layoutData = array(
 				'selector' => 'digicomCartPopup',
 				'params'   => array(
