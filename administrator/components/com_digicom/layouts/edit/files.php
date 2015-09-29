b6caa8a34a7543c222e635d05446317610f5c186
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-29 12:49:06 +0600

    files new structure code

--- administrator/components/com_digicom/layouts/edit/files.php
+++ administrator/components/com_digicom/layouts/edit/files.php
@@ -9,88 +9,107 @@
 
 // TODO : Translatable String
 defined('_JEXEC') or die;
-
-$document = JFactory::getDocument();
-$document->addScriptDeclaration('var DIGICOM_ALERT_REMOVE_FILES = "'. JText::_("COM_DIGICOM_PRODUCTS_FILES_REMOVE_WARNING") . '";');
-$document->addScript(JURI::root(true).'/media/com_digicom/js/repeatable-fields.js?v=1.0.0');
+JHtml::_('jquery.ui', array('sortable'));
+JText::script('COM_DIGICOM_PRODUCTS_FILES_REMOVE_WARNING');
+// $document = JFactory::getDocument();
+// addFilesRow();
+// $document->addScriptDeclaration('var DIGICOM_ALERT_REMOVE_FILES = "'. JText::_("COM_DIGICOM_PRODUCTS_FILES_REMOVE_WARNING") . '";');
+// $document->addScript(JURI::root(true).'/media/com_digicom/js/repeatable-fields.js?v=1.0.0');
+// $saveOrderingUrl = 'index.php?option=com_digicom&task=categories.saveOrderAjax&tmpl=component';
+// $saveOrderingUrl = '';
+// JHtml::_('sortablelist.sortable');
 ?>
+<table id="filesRowSample" class="hide">
+	<tbody><tr>
+			<td style="vertical-align: middle;">
+				<div class="text-center">
+					<span class="move"><i class="icon-move"></i></span>
+					<input type="hidden" name="jform[file][{{row-count-placeholder}}][id]" id="digicom_files_id" value="" />
+					<input type="hidden" name="jform[file][{{row-count-placeholder}}][ordering]" value="" id="files_row_count_placeholder_id_ordering"/>
+				</div>
+			</td>
+			<td>
+				<input type="text" name="jform[file][{{row-count-placeholder}}][name]" id="files_row_count_placeholder_id_name" placeholder="File Name"/>
+			</td>
+
+			<td>
+				<div class="input-prepend input-append" style="display: block;">
+					<input type="text" name="jform[file][{{row-count-placeholder}}][url]" id="files_row_count_placeholder_id_url" placeholder="Upload or enter the file URL" class="span8"/>
+					<a class="files_uploader_modal btn modal" title="Select"
+					href="javascript:;" onclick="openModal(this);"
+					>
+					Select</a>
+				</div>
+			</td>
+			<td style="vertical-align: middle;">
+				<div class="text-center">
+					<a href="#" class="remove" onclick="removeFilesRow(this);"><i class="icon-remove"></i></a>
+				</div>
+			</td>
+		</tr></tbody>
+</table>
 
 <fieldset class="adminform">
 	<legend><?php echo JText::_('COM_DIGICOM_PRODUCT_SINGLE_FILES');?></legend>
 	<div id="digicom_item_files_items" class="repeat">
-		<table class="table table-striped wrapper" id="itemList">
+
+		<table class="table table-striped wrapper" id="filesitemList">
 			<thead>
-				<tr class="row">
-					<th><i class="icon-menu-2"></i></th>
+				<tr>
+					<th>
+						<div class="text-center">
+							<i class="icon-menu-2"></i>
+						</div>
+					</th>
 					<th><?php echo JText::_('COM_DIGICOM_PRODUCT_FILE_NAME_LABEL');?></th>
-					<th><?php echo JText::_('COM_DIGICOM_PRODUCT_FILE_NAME_LABEL');?></th><th>File URL</th>
-					<th></th>
+					<th><?php echo JText::_('COM_DIGICOM_PRODUCT_FILE_URL_LABEL');?></th>
+					<th>
+						<div class="text-center">
+							<?php echo JText::_('COM_DIGICOM_ACTION');?>
+						</div>
+					</th>
+
 				</tr>
 			</thead>
-			<tbody class="container">
-				<tr class="template row">
-					<td>
-						<span class="move"><i class="icon-move"></i></span>
-						<input type="hidden" name="jform[file][{{row-count-placeholder}}][id]" id="digicom_files_id" value="" />
-					</td>
-
-					<td>
-						<input type="text" name="jform[file][{{row-count-placeholder}}][name]" id="files_row_count_placeholder_id_name" placeholder="File Name"/>
-					</td>
-
-					<td>
-						<div class="input-prepend input-append" style="display: block;">
-							<input type="text" name="jform[file][{{row-count-placeholder}}][url]" id="files_row_count_placeholder_id_url" placeholder="Upload or enter the file URL" class="span8"/>
-							<a class="files_uploader_modal btn modal" title="Select"
-							href="javascript:;" onclick="openModal(this);"
-							>
-							Select</a>
-						</div>
-					</td>
-
-					<td>
-						<span class="remove"><i class="icon-remove"></i></span>
-						<input type="hidden" name="jform[file][{{row-count-placeholder}}][ordering]" value="" id="files_row_count_placeholder_id_ordering"/>
-					</td>
-				</tr>
+			<tbody id="itemsfilesRows">
 				<?php
 					$form = $displayData->getForm();
 					$item = $displayData->get('item');
 					$form_data = $form->getData();
 					$files = $form_data->get('file');
 					if((isset($item->file) && count($item->file) >=1 && is_array($item->file)) or is_array($files) ):
-
 					foreach($files as $key => $value){
 						if(is_array($value)) $value = (object) $value;
 					?>
-					<tr class="row">
-						<td>
-							<span class="move"><i class="icon-move"></i></span>
-							<input type="hidden" name="jform[file][<?php echo $key; ?>][id]" id="digicom_files_id" value="<?php echo (isset($value->id) ? $value->id : ''); ?>" />
+					<tr data-index="<?php echo $key; ?>">
+						<td style="vertical-align: middle;">
+							<div class="text-center">
+								<span class="move"><i class="icon-move"></i></span>
+								<input type="hidden" name="jform[file][<?php echo $key; ?>][id]" id="digicom_files_id" value="<?php echo (isset($value->id) ? $value->id : ''); ?>" />
+								<input type="hidden" name="jform[file][<?php echo $key; ?>][ordering]" id="files_ordering_<?php echo $key; ?>"
+									value="<?php echo (isset($value->ordering) ? $value->ordering : ''); ?>" />
+							</div>
 						</td>
-
 						<td>
 							<input type="text" id="files_<?php echo $key; ?>_name"
-							name="jform[file][<?php echo $key; ?>][name]" placeholder="File Name" value="<?php echo (isset($value->name) ? $value->name : ''); ?>"/>
+							name="jform[file][<?php echo $key; ?>][name]" placeholder="File Name" value="<?php echo (isset($value->name) ? $value->name : ''); ?>" />
 						</td>
-
 						<td>
 							<div class="input-prepend input-append" style="display: block;">
 								<input type="text" name="jform[file][<?php echo $key; ?>][url]" id="files_<?php echo $key; ?>_url" placeholder="Upload or enter the file URL" class="span8"
-								value="<?php echo (isset($value->url) ? $value->url : ''); ?>"
-								/>
+								value="<?php echo (isset($value->url) ? $value->url : ''); ?>" />
+
 								<a class="files_uploader_modal btn modal" title="Select"
-								href="javascript:;" onclick="openModal(this);"
-								>
-								Select</a>
+									href="javascript:;" onclick="openModal(this);">
+
+									Select
+								</a>
 							</div>
 						</td>
-
-						<td>
-							<input type="hidden" name="jform[file][<?php echo $key; ?>][ordering]" id="files_ordering_<?php echo $key; ?>"
-								value="<?php echo (isset($value->ordering) ? $value->ordering : ''); ?>"
-								/>
-							<span class="remove"><i class="icon-remove"></i></span>
+						<td style="vertical-align: middle;">
+							<div class="text-center">
+								<a href="#" class="remove" onclick="removeFilesRow(this);"><i class="icon-remove"></i></a>
+							</div>
 						</td>
 					</tr>
 					<?php
@@ -99,9 +118,9 @@ $document->addScript(JURI::root(true).'/media/com_digicom/js/repeatable-fields.j
 				?>
 			</tbody>
 			<tfoot>
-				<tr class="row">
-					<td colspan="7">
-						<span class="add btn btn-success">Add File +</span>
+				<tr>
+					<td colspan="4">
+						<a href="#" onclick="addFilesRow();" class="add btn btn-success">Add File +</a>
 						<input type="hidden" name="jform[files_remove_id]" value="" id="jform_files_remove_id"/>
 					</td>
 				</tr>
