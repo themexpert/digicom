a85b76e5a339e7729ac97dde9c45799e83429afe
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-01 19:00:34 +0600

    gulp operation ready for release

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,50 @@ var gulp    = require('gulp');
 var zip     = require('gulp-zip');
 var rm      = require('gulp-rimraf');
 var replace = require('gulp-replace');
+var es      = require('event-stream');
 
 //release
-gulp.task('release', function () {
-  gulp.src('./releases', { read: false }).pipe(rm({ force: true }));
+gulp.task('cleanRelease', function () {
+  return gulp.src('./releases', { read: false }).pipe(rm({ force: true }));
+});
 
-  return gulp.src([
-		'!*',
-		'./src/**',
-	])
-  // run the replacement of version name from
-  .pipe(replace(/##VERSION##/g, extension.version))
-  .pipe(replace(/##CREATIONDATE##/g, extension.creationDate))
-  // .pipe(gulp.dest('releases/file.xml'));
-	.pipe(zip(extension.name + '_' + extension.version + '.zip'))
-	.pipe(gulp.dest('releases'));
+var modelFolders = [
+    'com_digicom',
+    'mod_digicom_cart',
+    'mod_digicom_categories',
+    'mod_digicom_menu',
+    'plg_content_digicom',
+    'plg_digicom_pay_offline',
+    'plg_digicom_pay_paypal',
+    'plg_editors-xtd_digicom',
+    'plg_finder_digicom',
+    'plg_system_digicom',
+];
+
+// identifies a dependent task must be complete before this one begins
+gulp.task('release', ['cleanRelease'], function() {
+    var zips = [], modelZip;
+
+    for (var i = 0; i < modelFolders.length; i++) {
+        var model = modelFolders[i];
+        modelZip = gulp.src('./src/'+ model+'/**')
+                       .pipe(replace(/##VERSION##/g, extension.version))
+                       .pipe(replace(/##CREATIONDATE##/g, extension.creationDate))
+                       .pipe(zip(model + '.zip'));
+        // notice we removed the dest step and store the zip stream (still in memory)
+        zips.push(modelZip);
+    }
+
+    var pkgfiles = gulp.src(['!*', './src/language', './src/pkg.script.php', './src/pkg_digicom.xml'])
+                        // run the replacement of version name
+                        .pipe(replace(/##VERSION##/g, extension.version))
+                        // run the replacement of creation date
+                        .pipe(replace(/##CREATIONDATE##/g, extension.creationDate));
+
+    zips.push(pkgfiles);
+
+    // we finally merge them (the zips), zip them again, and output.
+    return es.merge.apply(null, zips)
+        .pipe(zip(extension.name + '_' + extension.version + '.zip'))
+        .pipe(gulp.dest('releases'));
 });
