9dc5aba728b4cd70d707a1d2092b058556e7f272
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-02 12:43:13 +0600

    plugins copy n remove operation ready

--- joomla-gulp/plugins/plugins.js
+++ joomla-gulp/plugins/plugins.js
@@ -16,173 +16,141 @@ gulp.task('clean:' + baseTask,
 		'clean:' + baseTask + ':finder',
 		'clean:' + baseTask + ':system',
 		'clean:' + baseTask + ':offline',
-		'clean:' + baseTask + ':paypal'
+		'clean:' + baseTask + ':paypal',
+		'clean:' + baseTask + ':language'
 	],
 	function() {
 		return true;
 });
 
-// Copy: com
 gulp.task('clean:' +  baseTask + ':content', function() {
 	return gulp.src(config.wwwDir + '/plugins/content/digicom/', { read: false }).pipe(rm({ force: true }));
 });
-gulp.task('clean:' +  baseTask + ':xml', function() {
-	return gulp.src(config.wwwDir + '/administrator/components/com_xmonials/xml.php', { read: false })
-				.pipe(rm({ force: true }));
+
+gulp.task('clean:' +  baseTask + ':editor', function() {
+	return gulp.src(config.wwwDir + '/plugins/editors-xtd/digicom', { read: false }).pipe(rm({ force: true }));
 });
 
-// Clean administrator
-gulp.task('clean:' + baseTask + ':componentsadmin', function() {
-	return gulp.src(config.wwwDir + '/administrator/components/com_xmonials', { read: false })
-				.pipe(rm({ force: true }));
+gulp.task('clean:' + baseTask + ':finder', function() {
+	return gulp.src(config.wwwDir + '/plugins/finder/digicom/', { read: false }).pipe(rm({ force: true }));
 });
-// Clean site
-gulp.task('clean:' + baseTask + ':componentssite', function() {
-	return gulp.src(config.wwwDir + '/components/com_xmonials', { read: false })
-				.pipe(rm({ force: true }));
+
+gulp.task('clean:' + baseTask + ':system', function() {
+	return gulp.src(config.wwwDir + '/plugins/system/digicom/', { read: false }).pipe(rm({ force: true }));
 });
-// Clean: site languages
-gulp.task('clean:' + baseTask + ':langsite', function() {
-	return gulp.src(config.wwwDir + '/site/language/**/*.com_xmonials.*')
-		.pipe(rm({ force: true }));
+
+gulp.task('clean:' + baseTask + ':offline', function() {
+	return gulp.src(config.wwwDir + '/plugins/digicom_pay/offline/', { read: false }).pipe(rm({ force: true }));
 });
-// Clean: admin languages
-gulp.task('clean:' + baseTask + ':langadmin', function() {
-	return gulp.src(config.wwwDir + '/administrator/language/**/*.com_xmonials.*')
-		.pipe(rm({ force: true }));
+
+gulp.task('clean:' + baseTask + ':paypal', function() {
+	return gulp.src(config.wwwDir + '/plugins/digicom_pay/paypal/', { read: false }).pipe(rm({ force: true }));
 });
-// Clean Media
-gulp.task('clean:' + baseTask + ':media', function() {
-	return gulp.src(config.wwwDir + '/media/com_xmonials', { read: false })
-				.pipe(rm({ force: true }));
+
+gulp.task('clean:' + baseTask + ':language', function() {
+	return gulp.src(
+    [
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_content_digicom.ini',
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_digicom_pay_offline.ini',
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_digicom_pay_paypal.ini',
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_editors-xtd_digicom.ini',
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_finder_digicom.ini',
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_finder_digicom.sys.ini',
+      config.wwwDir + '/administrator/language/en-GB/en-GB.plg_system_digicom.ini'
+    ],
+    { read: false }
+  ).pipe(rm({ force: true }));
 });
+
 
 // Copy
 gulp.task('copy:' + baseTask,
 	[
-		'copy:' + baseTask + ':installscript',
-		'copy:' + baseTask + ':xml',
-		'copy:' + baseTask + ':componentsadmin',
-		'copy:' + baseTask + ':componentssite',
-		'copy:' + baseTask + ':langadmin',
-		'copy:' + baseTask + ':langsite',
-		'copy:' + baseTask + ':media'
+    'copy:' + baseTask + ':content',
+		'copy:' + baseTask + ':editor',
+		'copy:' + baseTask + ':finder',
+		'copy:' + baseTask + ':system',
+		'copy:' + baseTask + ':offline',
+		'copy:' + baseTask + ':paypal',
+		'copy:' + baseTask + ':language'
 	],
 	function() {
 });
 
-// Copy: com
-gulp.task('copy:' +  baseTask + ':installscript', ['clean:' + baseTask + ':installscript'], function() {
-	return gulp.src(extPath + '/script.php')
-		.pipe(gulp.dest(config.wwwDir + '/administrator/components/com_xmonials'));
-});
-gulp.task('copy:' +  baseTask + ':xml', ['clean:' + baseTask + ':xml'], function() {
-	return gulp.src(extPath + '/xmonials.xml')
-		.pipe(gulp.dest(config.wwwDir + '/administrator/components/com_xmonials'));
+gulp.task('copy:' +  baseTask + ':content', ['clean:' + baseTask + ':content'], function() {
+	return gulp.src([
+
+    extPath + '/**/content/digicom/**',
+
+  ]).pipe(gulp.dest(config.wwwDir + '/plugins/content/digicom'));
 });
 
-// Copy: admin component
-gulp.task('copy:' +  baseTask + ':componentsadmin', ['clean:' + baseTask + ':componentsadmin'], function() {
-	return gulp.src(extPath + '/admin/**')
-		.pipe(gulp.dest(config.wwwDir + '/administrator/components/com_xmonials'));
+gulp.task('copy:' +  baseTask + ':editor', ['clean:' + baseTask + ':editor'], function() {
+	return gulp.src(extPath + '/plugins/editors-xtd/digicom/**').pipe(gulp.dest(config.wwwDir + '/plugins/editors-xtd/digicom'));
 });
 
-// Copy: component
-gulp.task('copy:' +  baseTask + ':componentssite', ['clean:' + baseTask + ':componentssite'], function() {
-	return gulp.src(extPath + '/site/**')
-		.pipe(gulp.dest(config.wwwDir + '/components/com_xmonials'));
-});
-// Copy: admin languages
-gulp.task('copy:' +  baseTask + ':langadmin', ['clean:' + baseTask + ':langadmin'], function() {
-	return gulp.src(extPath + '/admin/language/**')
-		.pipe(gulp.dest(config.wwwDir + '/administrator/language'));
-});
-// Copy: site languages
-gulp.task('copy:' +  baseTask + ':langsite', ['clean:' + baseTask + ':langsite'], function() {
-	return gulp.src(extPath + '/site/language/**')
-		.pipe(gulp.dest(config.wwwDir + '/language'));
-});
-// Copy: media
-gulp.task('copy:' +  baseTask + ':media', ['clean:' + baseTask + ':media'], function() {
-	return gulp.src(mediaPath + '/**')
-		.pipe(gulp.dest(config.wwwDir + '/media/com_xmonials'));
+gulp.task('copy:' +  baseTask + ':finder', ['clean:' + baseTask + ':finder'], function() {
+	return gulp.src(extPath + '/plugins/finder/digicom/**').pipe(gulp.dest(config.wwwDir + '/plugins/finder/digicom'));
 });
 
-// less
-gulp.task('less:' + baseTask, function () {
-	return gulp.src(mediaPath + '/less/style.less')
-		.pipe(less({loadPath: [mediaPath + '/less']}))
-		.pipe(gulp.dest(mediaPath + '/css'))
-		.pipe(gulp.dest(config.wwwDir + '/media/com_xmonials/css'))
-		.pipe(minifyCSS())
-		.pipe(rename(function (path) {
-				path.basename += '.min';
-		}))
-		.pipe(gulp.dest(mediaPath + '/css'))
-		.pipe(gulp.dest(config.wwwDir + '/media/com_xmonials/css'));
+gulp.task('copy:' +  baseTask + ':system', ['clean:' + baseTask + ':system'], function() {
+	return gulp.src(extPath + '/plugins/system/digicom/**').pipe(gulp.dest(config.wwwDir + '/plugins/system/digicom'));
 });
 
-// zip
-gulp.task('zip:' + baseTask, function () {
-	return gulp.src(mediaPath + '/less/style.less')
-		.pipe(less({loadPath: [mediaPath + '/less']}))
-		.pipe(gulp.dest(mediaPath + '/css'))
-		.pipe(gulp.dest(config.wwwDir + '/media/com_xmonials/css'))
-		.pipe(minifyCSS())
-		.pipe(rename(function (path) {
-				path.basename += '.min';
-		}))
-		.pipe(gulp.dest(mediaPath + '/css'))
-		.pipe(gulp.dest(config.wwwDir + '/media/com_xmonials/css'));
+gulp.task('copy:' +  baseTask + ':offline', ['clean:' + baseTask + ':offline'], function() {
+	return gulp.src(extPath + '/plugins/digicom_pay/offline/**').pipe(gulp.dest(config.wwwDir + '/plugins/digicom_pay/offline'));
+});
+
+gulp.task('copy:' +  baseTask + ':paypal', ['clean:' + baseTask + ':paypal'], function() {
+	return gulp.src(extPath + '/plugins/digicom_pay/paypal/**').pipe(gulp.dest(config.wwwDir + '/plugins/digicom_pay/paypal'));
+});
+
+gulp.task('copy:' +  baseTask + ':language', ['clean:' + baseTask + ':language'], function() {
+  //extPath + '/plg_**/language/en-GB/**',
+	return gulp.src([
+    extPath + '/plg_content_digicom/language/en-GB/**',
+    extPath + '/plg_digicom_pay_offline/language/en-GB/**',
+    extPath + '/plg_digicom_pay_paypal/language/en-GB/**',
+    extPath + '/plg_editors-xtd_digicom/language/en-GB/**',
+    extPath + '/plg_finder_digicom/language/en-GB/**',
+    extPath + '/plg_system_digicom/language/en-GB/**',
+  ]).pipe(gulp.dest(config.wwwDir + '/administrator/language/en-GB'));
 });
 
 // Watch
 gulp.task('watch:' + baseTask,
 	[
-		'watch:' + baseTask + ':installscript',
-		'watch:' + baseTask + ':xml',
-		'watch:' + baseTask + ':componentsadmin',
-		'watch:' + baseTask + ':componentssite',
-		'watch:' + baseTask + ':langadmin',
-		'watch:' + baseTask + ':langsite',
-		'watch:' + baseTask + ':media',
-		'watch:' + baseTask + ':less'
+    'watch:' + baseTask + ':content',
+		'watch:' + baseTask + ':editor',
+		'watch:' + baseTask + ':finder',
+		'watch:' + baseTask + ':system',
+		'watch:' + baseTask + ':offline',
+		'watch:' + baseTask + ':paypal'
 	],
 	function() {
 		return true;
 });
 
-// watch: com
-gulp.task('watch:' +  baseTask + ':installscript', function() {
-		gulp.watch(extPath + '/script.php', ['copy:' + baseTask + ':installscript', browserSync.reload]);
-});
-gulp.task('watch:' +  baseTask + ':xml', function() {
-		gulp.watch(extPath + '/xmonials.xml', ['copy:' + baseTask + ':xml', browserSync.reload]);
+gulp.task('watch:' +  baseTask + ':content', function() {
+	gulp.watch(extPath + '/plugins/content/digicom/**', ['copy:' + baseTask + ':content', browserSync.reload]);
 });
 
-// watch: admin component
-gulp.task('watch:' +  baseTask + ':componentsadmin', function() {
-		gulp.watch(extPath + '/admin/**', ['copy:' + baseTask + ':componentsadmin', browserSync.reload]);
-});
-// Copy: component
-gulp.task('watch:' +  baseTask + ':componentssite', function() {
-	gulp.watch(extPath + '/site/**', ['copy:' + baseTask + ':componentssite', browserSync.reload]);
+gulp.task('watch:' +  baseTask + ':editor', function() {
+	gulp.watch(extPath + '/plugins/editors-xtd/digicom/**', ['copy:' + baseTask + ':editor', browserSync.reload]);
 });
 
-// Watch: Languagesadmin
-gulp.task('watch:' + baseTask + ':langadmin', function() {
-	gulp.watch(extPath + '/admin/language/**', ['copy:' + baseTask + ':langadmin', browserSync.reload]);
-});
-// Watch: Languages site
-gulp.task('watch:' + baseTask + ':langsite', function() {
-	gulp.watch(extPath + '/site/language/**', ['copy:' + baseTask + ':langsite', browserSync.reload]);
+gulp.task('watch:' +  baseTask + ':finder', function() {
+	gulp.watch(extPath + '/plugins/finder/digicom/**', ['copy:' + baseTask + ':finder', browserSync.reload]);
 });
 
-// Watch: media
-gulp.task('watch:' + baseTask + ':media', function() {
-	gulp.watch(mediaPath + '/**', ['copy:' + baseTask + ':media', browserSync.reload]);
+gulp.task('watch:' +  baseTask + ':system', function() {
+	gulp.watch(extPath + '/plugins/system/digicom/**', ['copy:' + baseTask + ':system', browserSync.reload]);
 });
-	// Watch: Styles
-gulp.task('watch:' + baseTask + ':less', function() {
-		gulp.watch(mediaPath + '/less/**', ['less:' + baseTask + ':less', browserSync.reload]);
+
+gulp.task('watch:' +  baseTask + ':offline', function() {
+	gulp.watch(extPath + '/plugins/digicom_pay/offline/**', ['copy:' + baseTask + ':offline', browserSync.reload]);
+});
+
+gulp.task('watch:' +  baseTask + ':paypal', function() {
+	gulp.watch(extPath + '/plugins/digicom_pay/paypal/**', ['copy:' + baseTask + ':paypal', browserSync.reload]);
 });
