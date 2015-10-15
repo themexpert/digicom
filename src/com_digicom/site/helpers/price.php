5fb5ab6e31a084bc208576372a6b10961b154578
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-15 18:30:26 +0600

    price helper updated to calculate tax

--- src/com_digicom/site/helpers/price.php
+++ src/com_digicom/site/helpers/price.php
@@ -35,6 +35,123 @@ class DigiComSiteHelperPrice {
 		return $price;
 	}
 
+
+	/**
+	* price formet helper
+	*/
+	public static function tax_price($price, $configs)
+	{
+		if($price <= 0) return;
+		// check the tax info
+		$tax_enable 						= $configs->get('enable_taxes', 0);
+		$display_tax_with_price = $configs->get('display_tax_with_price', 0);
+		$price_with_tax 				= $configs->get('price_with_tax', 0);
+		$fallback_tax_rate 			= $configs->get('fallback_tax_rate', 0);
+
+		$tax_rate = self::get_tax_rate($configs);
+		$tax_rate = $tax_rate * 100;
+
+		if(!$tax_enable)
+		{
+			return '';
+		}
+		elseif($display_tax_with_price && $price_with_tax)
+		{
+			$price = JText::sprintf('COM_DIGICOM_PRICE_WITH_TAX_INCLUDE', $tax_rate);
+			return $price;
+		}
+		elseif($display_tax_with_price && !$price_with_tax)
+		{
+			$price = JText::sprintf('COM_DIGICOM_PRICE_WITH_TAX_EXCLUDE', $tax_rate);
+			return $price;
+		}
+	}
+
+
+
+	/**
+	 * Get taxation rate
+	 *
+	 * @since 1.3.3
+	 * @param bool $country
+	 * @param bool $state
+	 * @return mixed|void
+	 */
+	public static function get_tax_rate($configs, $country = false, $state = false )
+	{
+		$rate = (float) $configs->get('fallback_tax_rate', 0 );
+
+		$customer = new DigiComSiteHelperSession();
+
+		// print_r($user_address);die;
+		// return;
+		if( empty( $country ) ) {
+			if( $customer->_user->id < 1 && ! empty( $customer ) ) {
+				$country = $customer->_customer->country;
+			}
+			$country = ! empty( $country ) ? $country : $configs->get('base_country','');
+		}
+
+		if( empty( $state ) ) {
+			if( $customer->_user->id < 1 && ! empty( $customer ) ) {
+				$state = $customer->_customer->state;
+			}
+			$state = ! empty( $state ) ? $state : $configs->get('base_state','');
+		}
+
+		if( ! empty( $country ) ) {
+			$tax_rates   = $configs->get('tax_rates',array());
+			$json = json_decode($tax_rates, true);
+			$tax_rates = self::group_by_key($json);
+			if( ! empty( $tax_rates ) ) {
+			// print_r($tax_rates);die;
+				// Locate the tax rate for this country / state, if it exists
+				foreach( $tax_rates as $key => $tax_rate ) {
+
+					if( $country != $tax_rate['country'] )
+						continue;
+
+					if( ! empty( $tax_rate['all_states'] ) ) {
+						if( ! empty( $tax_rate['rate'] ) ) {
+							$rate = number_format( $tax_rate['rate'], 4 );
+						}
+					} else {
+
+						if( empty( $tax_rate['state'] ) || strtolower( $state ) != strtolower( $tax_rate['state'] ) )
+							continue;
+
+						$state_rate = $tax_rate['rate'];
+						if( 0 !== $state_rate || ! empty( $state_rate ) ) {
+							$rate = number_format( $state_rate, 4 );
+						}
+					}
+				}
+			}
+		}
+		// echo $rate;die;
+		if( $rate > 1 ) {
+			// Convert to a number we can use
+			$rate = $rate / 100;
+		}
+
+		return $rate;
+	}
+
+	public static function group_by_key($array)
+	{
+	    $result = array();
+
+	    foreach ($array as $key=>$sub)
+	    {
+	        // print_r($key);die;
+					foreach ($sub as $k => $v)
+	        {
+	            $result[$k][$key] = $v;
+	        }
+	    }
+	    return $result;
+	}
+
 	/*
 	* $item = product object
 	* show product subscription or validity period for purchase
