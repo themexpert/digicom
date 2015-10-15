5a9d7f7e5dd75007bbc028f02f3a265e6c613a2a
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-15 20:00:11 +0600

    cart price view new structure

--- src/com_digicom/site/templates/default/cart_price.php
+++ src/com_digicom/site/templates/default/cart_price.php
@@ -8,66 +8,31 @@
  */
 
 defined('_JEXEC') or die;
-$table_column = 4;
 ?>
-<div class="dc-cart-price table-responsive">
+<div class="well dc-cart-coupon clearfix">
+  <div class="pull-right input-group" style="width:50%;">
+    <input
+    type="text" data-digicom-id="promocode"
+    name="promocode"
+    class="form-control"
+    placeholder="<?php echo JText::_("COM_DIGICOM_CART_COUPON"); ?>"
+    value="<?php echo $this->promocode; ?>"
+    />
+    <div class="input-group-btn">
+      <button class="btn btn-default" type="submit" onclick="Digicom.refreshCart();">
+        <i class="ico-gift"></i> <?php echo JText::_("COM_DIGICOM_CART_PROMOCODE_APPLY"); ?>
+      </button>
+    </div>
+  </div>
+  <p class="lead no-margin">
+    <?php echo JText::_("COM_DIGICOM_CART_IF_PROMOCODE_LABEL"); ?>
+  </p>
+</div>
+
+<div class="dc-cart-price">
   <table id="dc-cart-price-table" class="table well" width="100%">
-    <tr valign="top">
-      <td class="general_text" colspan="<?php echo $table_column-1; ?>" valign="bottom">
-        <?php echo JText::_("COM_DIGICOM_CART_IF_PROMOCODE_LABEL"); ?>
-      </td>
-      <td>
-          <div class="dc-cart-subtotal-title text-right">
-            <?php echo JText::_("COM_DIGICOM_SUBTOTAL");?>
-          </div>
-      </td>
-      <td>
-          <div class="dc-cart-subtotal-price text-right" data-digicom-id="cart_subtotal" style="font-size: 15px;text-align:right;">
-            <?php echo DigiComSiteHelperPrice::format_price($this->tax['subtotal'], $this->tax['currency'], true, $this->configs); ?>
-          </div>
-      </td>
-    </tr>
-
-    <tr valign="top">
-      <td colspan="<?php echo $table_column - 1; ?>" >
-        <div class="input-group">
-          <input type="text" data-digicom-id="promocode" name="promocode" class="form-control" value="<?php echo $this->promocode; ?>" />
-          <div class="input-group-btn">
-            <button class="btn btn-default" type="submit" onclick="Digicom.refreshCart();">
-              <i class="ico-gift"></i> <?php echo JText::_("COM_DIGICOM_CART_PROMOCODE_APPLY"); ?>
-            </button>
-          </div>
-        </div>
-      </td>
-      <td nowrap="nowrap" style="text-align: center;">
-        <ul class="list-unstyled">
-          <?php if ( $this->tax['discount_calculated'] && !$this->tax['item_discount'] ): ?>
-            <li class="dc-cart-discount-title" style="font-size: 15px;text-align:right;">
-            <?php echo JText::_("COM_DIGICOM_PROMO_DISCOUNT");?>
-          </li>
-          <?php endif; ?>
-
-          <li class="dc-cart-total-title" style="font-weight: bold;font-size: 18px;text-align:right;">
-            <?php echo JText::_("COM_DIGICOM_TOTAL");?>
-          </li>
-        </ul>
-      </td>
-      <td nowrap="nowrap" style="text-align: center;">
-        <ul style="margin: 0; padding: 0;list-style-type: none;">
-          <?php if ($this->tax['discount_calculated'] && !$this->tax['item_discount']): ?>
-          <li class="dc-cart-discount-price" data-digicom-id="cart_discount" style="font-size: 15px;text-align:right;">
-            <?php echo DigiComSiteHelperPrice::format_price($this->tax['promo'], $this->tax['currency'], true, $this->configs); ?>
-          </li>
-          <?php endif; ?>
-
-          <li class="dc-cart-total-price" data-digicom-id="cart_total" style="font-weight: bold;font-size: 18px;text-align:right;">
-            <?php echo DigiComSiteHelperPrice::format_price($this->tax['taxed'], $this->tax['currency'], true, $this->configs); ?>
-          </li>
-        </ul>
-      </td>
-    </tr>
     <tr>
-      <td colspan="<?php echo $table_column - 1; ?>">
+      <td>
         <?php if($this->configs->get('askterms',0) == '1' && ($this->configs->get('termsid') > 0)):?>
           <div class="dc-accept-terms">
             <?php $agreeterms = JFactory::getApplication()->input->get("agreeterms", ""); ?>
@@ -89,7 +54,7 @@ $table_column = 4;
       </td>
     </tr>
     <tr>
-      <td colspan="<?php echo $table_column + 1; ?>">
+      <td colspan="3">
         <div class="text-right">
           <button
             type="button"
