c7b3ccf5f0a0ad010c2d72190b5bf010bbe3f087
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-18 15:54:40 +0600

    update sql test

--- src/com_digicom/admin/sql/updates/mysql/1.2.0.sql
+++ src/com_digicom/admin/sql/updates/mysql/1.2.0.sql
@@ -1,4 +1,4 @@
 # digicom 1.2.0 release sql update
 ALTER TABLE  `#__digicom_orders`
-ADD  `tax` DECIMAL( 13, 3 ) NOT NULL AFTER  `amount`,
+ADD `tax` DECIMAL( 13, 3 ) NOT NULL AFTER `amount`,
 CHANGE `price` `price` DECIMAL( 13, 3 ) NOT NULL DEFAULT '0.000' COMMENT 'original price without discount and tax';
