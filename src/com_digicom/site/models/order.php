a46cc0f5ea353a83f4d70180456e1517b8a6103e
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-29 20:59:53 +0600

    site order model refactor

--- src/com_digicom/site/models/order.php
+++ src/com_digicom/site/models/order.php
@@ -11,66 +11,94 @@ defined('_JEXEC') or die;
 
 class DigiComModelOrder extends JModelItem
 {
-	var $_id = null;
 
-	function __construct () {
-		parent::__construct();
-		$id = JRequest::getVar('id', 0);
-		$this->setId((int)$id);
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @since   1.0.0
+	 *
+	 * @return void
+	 */
+	protected function populateState()
+	{
+		$app = JFactory::getApplication('site');
+
+		// Load state from the request.
+		$pk = $app->input->getInt('id');
+		$this->setState('order.id', $pk);
+
+		// Load the parameters.
+		$params = $app->getParams();
+		$this->setState('params', $params);
+
 	}
 
-	function setId($id) {
-		$this->_id = $id;
-		$this->_order = null;
-	}
-
-	function getOrder($id = 0) {
-		$custommer = new DigiComSiteHelperSession();
-
-		if (empty ($this->_order)) {
-			$db = JFactory::getDBO();
-			if ($id > 0) $this->_id = $id;
-			else $id = $this->_id;
-
-			$query = $db->getQuery(true);
-			$query->select('o.*')
-				  ->from($db->quoteName('#__digicom_orders','o'))
-				  ->where($db->quoteName('o.id').'='.intval($id))
-				  ->where($db->quoteName('o.published').'='.'1');
-
-
-			// $sql = "SELECT o.*"
-			// 		." FROM #__digicom_orders o"
-			// 		." WHERE o.id='".intval($id)."' AND o.published='1'"
-			// ;
-			$db->setQuery($query);
-			$this->_order = $db->loadObject();
-
-			$db->clear();
-			$query = $db->getQuery(true);
-			$query->select($db->quoteName('p').'.*')
-				  ->select($db->quoteName('od.quantity'))
-				  ->select($db->quoteName('od.package_type'))
-				  ->select($db->quoteName('od.price', 'price'))
-				  ->select($db->quoteName('od.userid'))
-				  ->from($db->quoteName('#__digicom_products','p'))
-				  ->from($db->quoteName('#__digicom_orders_details','od'))
-				  ->where($db->quoteName('p.id').'='.$db->quoteName('od.productid'))
-				  ->where($db->quoteName('od.orderid').'='.$db->quote($this->_order->id));
-			$db->setQuery($query);
-			$prods = $db->loadObjectList();
-
-
-			// $sql = "SELECT p.*, od.package_type, od.amount_paid as price , od.userid";
-			// $sql .= " FROM #__digicom_products as p, #__digicom_orders_details as od 
-			// WHERE p.id=od.productid AND od.orderid='". $this->_order->id ."'";
-			// $db->setQuery($sql);
-			//$prods = $db->loadObjectList();
-
-			$this->_order->products = $prods;
+	/**
+	 * Method to get product data.
+	 *
+	 * @param   integer  $pk  The id of the product.
+	 *
+	 * @return  mixed  Menu item data object on success, false on failure.
+	 */
+	public function getItem($pk = null)
+	{
+		$pk = (!empty($pk)) ? $pk : (int) $this->getState('order.id');
+		if ($this->_item === null)
+		{
+			$this->_item = array();
 		}
-		//print_r($this->_order);die;
-		return $this->_order;
+
+		if (!isset($this->_item[$pk]))
+		{
+			try
+			{
+				$db = JFactory::getDBO();
+				$query = $db->getQuery(true);
+				$query->select('o.*')
+					  ->from($db->quoteName('#__digicom_orders','o'))
+					  ->where($db->quoteName('o.id').'='.intval($pk))
+					  ->where($db->quoteName('o.published').'='.'1');
+
+				$db->setQuery($query);
+				$data = $db->loadObject();
+
+				$db->clear();
+				$query = $db->getQuery(true);
+				$query->select($db->quoteName('p').'.*')
+					  ->select($db->quoteName('od.quantity'))
+					  ->select($db->quoteName('od.package_type'))
+					  ->select($db->quoteName('od.price', 'price'))
+					  ->select($db->quoteName('od.userid'))
+					  ->from($db->quoteName('#__digicom_products','p'))
+					  ->from($db->quoteName('#__digicom_orders_details','od'))
+					  ->where($db->quoteName('p.id').'='.$db->quoteName('od.productid'))
+					  ->where($db->quoteName('od.orderid').'='.$db->quote($data->id));
+				$db->setQuery($query);
+				$prods = $db->loadObjectList();
+
+				$data->products = $prods;
+
+				$this->_item[$pk] = $data;
+			}
+			catch (Exception $e)
+			{
+				if ($e->getCode() == 404)
+				{
+					// Need to go thru the error handler to allow Redirect to work.
+					JError::raiseError(404, $e->getMessage());
+				}
+				else
+				{
+					$this->setError($e);
+					$this->_item[$pk] = false;
+				}
+			}
+		}
+
+		return $this->_item[$pk];
+
 	}
 
 }
