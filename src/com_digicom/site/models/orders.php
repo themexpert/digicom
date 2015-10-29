2cdd6c19b45baebed63a2ae8bb3f8c6f26251fb3
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-10-29 20:59:37 +0600

    site orders model refactor

--- src/com_digicom/site/models/orders.php
+++ src/com_digicom/site/models/orders.php
@@ -11,29 +11,69 @@ defined('_JEXEC') or die;
 
 class DigiComModelOrders extends JModelList
 {
-	function __construct () {
-		parent::__construct();
-	}
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * This method should only be called once per instantiation and is designed
+	 * to be called on the first call to the getState() method unless the model
+	 * configuration flag to ignore the request is set.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 *
+	 * @since   12.2
+	 */
+	protected function populateState($ordering = 'ordering', $direction = 'ASC')
+	{
+		$app = JFactory::getApplication();
 
-	function getlistOrders(){
+		$params = $app->getParams();
+		$this->setState('params', $params);
+
+		parent::populateState($ordering, $direction);
+	}
+	/**
+	 * Get the master query for retrieving a list of products subject to the model state.
+	 *
+	 * @return  JDatabaseQuery
+	 *
+	 * @since   1.6
+	 */
+	protected function getListQuery()
+	{
+		$custommer = new DigiComSiteHelperSession();
 		$input = JFactory::getApplication()->input;
 		$search = $input->get('search','');
-		$custommer = new DigiComSiteHelperSession();
-		//print_r($custommer);die;
-		$db = JFactory::getDBO();
-		if (empty ($this->_orders)) {
-				
-			$sql = "SELECT o.*"
-					." FROM #__digicom_orders o, #__digicom_orders_details od"
-					." WHERE ".($search ? 'o.id = "'.$search.'" and ' : '')."o.userid=".$custommer->_customer->id." group by o.id order by o.id desc";
-			$this->_orders = $this->_getList($sql);
-			
-			//print_r($this->_orders);die;
-			
-		}
-		return $this->_orders;
+
+		$sql = "SELECT o.*"
+				." FROM #__digicom_orders o, #__digicom_orders_details od"
+				." WHERE ".($search ? 'o.id = "'.$search.'" and ' : '')."o.userid=".$custommer->_customer->id." group by o.id order by o.id desc";
+
+		return $sql;
 	}
+	//
+	// function getlistOrders(){
+	// 	$input = JFactory::getApplication()->input;
+	// 	$search = $input->get('search','');
+	// 	$custommer = new DigiComSiteHelperSession();
+	// 	//print_r($custommer);die;
+	// 	$db = JFactory::getDBO();
+	// 	if (empty ($this->_orders)) {
+	//
+	// 		$sql = "SELECT o.*"
+	// 				." FROM #__digicom_orders o, #__digicom_orders_details od"
+	// 				." WHERE ".($search ? 'o.id = "'.$search.'" and ' : '')."o.userid=".$custommer->_customer->id." group by o.id order by o.id desc";
+	// 		$this->_orders = $this->_getList($sql);
+	//
+	// 		//print_r($this->_orders);die;
+	//
+	// 	}
+	// 	return $this->_orders;
+	// }
 
 
 }
-
