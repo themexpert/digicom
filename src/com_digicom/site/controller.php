28f4f9533f4f3fbeabf30e34515829c3e6c27fde
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2016-05-25 16:49:05 +0600

    cache params updated for frontend

--- src/com_digicom/site/controller.php
+++ src/com_digicom/site/controller.php
@@ -39,7 +39,7 @@ class DigiComController extends JControllerLegacy
 		$vName = $this->input->get('view', 'categories');
 		$this->input->set('view', $vName);
 
-		if ($user->get('id') || $this->input->getMethod() == 'POST' || 'cart' == $vName || 'checkout' == $vName || 'thankyou' == $vName)
+		if (!$user->guest || ($this->input->getMethod() == 'POST' && ('cart' == $vName || 'checkout' == $vName || 'thankyou' == $vName || 'register' == $vName)))
 		{
 			$cachable = false;
 		}
