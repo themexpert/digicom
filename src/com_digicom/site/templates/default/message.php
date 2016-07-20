5767f37a975448a1359ff1891da2a69e9e61f48b
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2016-07-20 13:16:48 +0600

    only message layout added

---
+++ src/com_digicom/site/templates/default/message.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+?>
+<div id="digicom" class="dc dc-registration-complete">
+  <?php if ($this->params->get('show_page_heading')) : ?>
+  <h1>
+    <?php echo $this->escape($this->params->get('page_heading')); ?>
+  </h1>
+  <?php endif; ?>
+</div>
