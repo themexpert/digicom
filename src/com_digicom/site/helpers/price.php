7d2941f42075917a4a007ffe649cce1c2bb688ed
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2017-07-17 18:30:41 +0800

    new event context `com_digicom.common` and event onDigicomCalculateProductValidity on calculate validity

--- src/com_digicom/site/helpers/price.php
+++ src/com_digicom/site/helpers/price.php
@@ -178,37 +178,60 @@ class DigiComSiteHelperPrice {
 	* show product subscription or validity period for purchase
 	*/
 	public static function getProductValidityPeriod($item){
-		//print_r($item);die;
-		if(
-			!isset( $item->price_type )
-			or
-			( $item->price_type == 0 )
-		) return JText::_('COM_DIGICOM_PRODUCT_EXPIRATION_NEVER');
 
-		$expiration_type = $item->expiration_type;
+		$dispatcher	= JEventDispatcher::getInstance();
 
+		// Confirm that we have a price type
+		if(!isset( $item->price_type )) $item->price_type = 0;
+
+		// now switch by type to show message
+		switch ($item->price_type) {
+			case '0':
+				// forever
+				$result = JText::_('COM_DIGICOM_PRODUCT_EXPIRATION_NEVER');
+				break;
+
+			case '1':
+				// has expiration
+				$result = DigiComSiteHelperPrice::calculateExpirationLength($item->expiration_type, $item->expiration_length);
+				break;
+			
+			default:
+				// we dont know, lets trigger plugin
+				$result = DigiComSiteHelperPrice::calculateExpirationLength($item->expiration_type, $item->expiration_length);
+				$dispatcher->trigger('onDigicomCalculateProductValidity', array('com_digicom.common', &$item, &$result));
+				
+				break;
+		}
+
+		return $result;
+	}
+
+	public static function calculateExpirationLength($expiration_type, $expiration_length){
+
+		
 		switch ($expiration_type) {
 			case 'year':
-				if($item->expiration_length > 1){
-					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_YEARS');
+				if($expiration_length > 1){
+					return $expiration_length . ' ' . JText::_('COM_DIGICOM_YEARS');
 				}else {
-					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_YEAR');
+					return $expiration_length . ' ' . JText::_('COM_DIGICOM_YEAR');
 				}
 				break;
 
 			case 'day':
-				if($item->expiration_length > 1){
-					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_DAYS');
+				if($expiration_length > 1){
+					return $expiration_length . ' ' . JText::_('COM_DIGICOM_DAYS');
 				}else {
-					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_DAY');
+					return $expiration_length . ' ' . JText::_('COM_DIGICOM_DAY');
 				}
 				break;
 
 			case 'month':
-				if($item->expiration_length > 1){
-					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_MONTHS');
+				if($expiration_length > 1){
+					return $expiration_length . ' ' . JText::_('COM_DIGICOM_MONTHS');
 				}else {
-					return $item->expiration_length . ' ' . JText::_('COM_DIGICOM_MONTH');
+					return $expiration_length . ' ' . JText::_('COM_DIGICOM_MONTH');
 				}
 				break;
 			default:
