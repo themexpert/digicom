b0e2ce3f33af6ee1adb5b1693aa6ecc5a0d858df
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2017-09-05 22:32:03 +0800

    new test payment

---
+++ src/plg_digicom_pay_test/test.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+
+require_once(dirname(__FILE__) . '/test/helper.php');
+
+class plgDigiCom_PayTest extends JPlugin
+{
+	/**
+	 * Load the language file on instantiation. Note this is only available in Joomla 3.1 and higher.
+	 * If you want to support 3.0 series you must override the constructor
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
+	/*
+	* construct method
+	* default joomla plugin params
+	* initialize responseStatus for payment use
+	*/
+	function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		//Define Payment Status codes in Authorise  And Respective Alias in Framework
+		//1 = Approved, 2 = Declined, 3 = Error, 4 = Held for Review
+		$this->responseStatus= array(
+			'Success' => 'A',
+			'Failure' => 'C',
+			'Pending' => 'P'
+		);
+	}
+
+	/*
+	* method buildLayoutPath
+	* @layout = ask for tmpl file name, default is default, but can be used others name
+	* return propur file to take htmls
+	*/
+	function buildLayoutPath($layout)
+	{
+		if(empty($layout)) $layout = "default";
+
+		$app = JFactory::getApplication();
+
+		// core path
+		$core_file 	= dirname(__FILE__) . '/' . $this->_name . '/tmpl/' . $layout . '.php';
+
+		// override path from site active template
+		$override	= JPATH_BASE .'/templates/' . $app->getTemplate() . '/html/plugins/' . $this->_type . '/' . $this->_name . '/' . $layout . '.php';
+
+		if(JFile::exists($override))
+		{
+			$file = $override;
+		}
+		else
+		{
+	  		$file =  $core_file;
+		}
+
+		return $file;
+
+	}
+
+	/*
+	* method buildLayout
+	* @vars = object with product, order, user info
+	* @layout = tmpl name
+	* Builds the layout to be shown, along with hidden fields.
+	* @return html
+	*/
+
+	function buildLayout($vars, $layout = 'default' )
+	{
+
+		//Load the layout & push variables
+		ob_start();
+		$layout = $this->buildLayoutPath($layout);
+		include($layout);
+		$html = ob_get_contents();
+		ob_end_clean();
+		return $html;
+	}
+
+	/*
+	* method onDigicom_PayGetHTML
+	* on transection process this function is being used to get html from component
+	* @dependent : self::buildLayout()
+	* @return html for view
+	* @vars : passed from component, all info regarding payment n order
+	*/
+	function onDigicom_PayGetHTML($vars,$pg_plugin)
+	{
+		if($pg_plugin != $this->_name) return;
+		$vars->custom_name= $this->params->get( 'plugin_name' );
+		$vars->custom_email=$this->params->get( 'plugin_mail' );
+		$html = $this->buildLayout($vars);
+		return $html;
+	}
+
+	/*
+	* method onDigicom_PayGetInfo
+	* can be used Build List of Payment Gateway in the respective Components
+	* for payment process its not used
+	*/
+
+	function onDigicom_PayGetInfo($config)
+	{
+
+		if(!in_array($this->_name,$config)) return;
+		$obj 		= new stdClass;
+		$obj->name 	= $this->params->get( 'plugin_name' );
+		$obj->id	= $this->_name;
+		return $obj;
+	}
+
+	/*
+	* method onDigicom_PayProcesspayment
+	* used when we recieve payment from site or thurd party
+	* @data : the necessary info recieved from form about payment
+	* @return payment process final status
+	*/
+	function onDigicom_PayProcesspayment($data)
+	{
+		$processor = JFactory::getApplication()->input->get('processor','');
+		if($processor != $this->_name) return;
+
+		$payment_status = $this->translateResponse('Success');
+		$data['payment_status'] = $payment_status;
+
+		if(!isset($data['payment_status']))
+		{
+			$info = array('raw_data'	=>	$data);
+			$this->onDigicom_PayStorelog($this->_name, $info);
+		}
+
+		$result = array(
+			'transaction_id'	=>	$this->getUniqueTransactionId($data['order_id']),
+			'order_id'			=>	$data['order_id'],
+			'status'			=>	$payment_status,
+			'total_paid_amt'	=>	$data['total'],
+			'raw_data'			=>	json_encode($data),
+			'error'				=>	'',
+			'return'			=>	$data['return'],
+			'comment'			=>	$data['comment'],
+			'processor'			=>	'test'
+		);
+		return $result;
+	}
+
+	/*
+	* method translateResponse
+	* used to set proper sesponce for order status
+	* @invoice_status : payment status recieved from payment site: processor
+	* @return order status
+	*/
+	function translateResponse($invoice_status){
+
+		foreach($this->responseStatus as $key=>$value)
+		{
+			if($key==$invoice_status)
+			return $value;
+		}
+	}
+
+	/*
+	* method onDigicom_PayStorelog
+	* used to store log for plugin debug payment
+	* @data : the necessary info recieved from form about payment
+	* @return null
+	*/
+	function onDigicom_PayStorelog($name, $data)
+	{
+		if($name != $this->_name) return;
+		plgDigiCom_PayTestHelper::Storelog($this->_name,$data);
+	}
+
+	/*
+	* method getUniqueTransactionId
+	* used for local perpose to generate transection id
+	* @order_id : order_id
+	* @return long 15ch code
+	*/
+	function getUniqueTransactionId($order_id){
+		$uniqueValue = $order_id.time();
+		$long = md5(uniqid($uniqueValue, true));
+		return substr($long, 0, 15);
+	}
+}
---
+++ src/plg_digicom_pay_test/test/tmpl/default.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @package		DigiCom
+ * @author 		ThemeXpert http://www.themexpert.com
+ * @copyright	Copyright (c) 2010-2015 ThemeXpert. All rights reserved.
+ * @license 	GNU General Public License version 3 or later; see LICENSE.txt
+ * @since 		1.0.0
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('jquery.framework');
+JHTML::_('behavior.formvalidation');
+$configs = JComponentHelper::getComponent('com_digicom')->params;
+
+$document = JFactory::getDocument();
+if($vars->custom_email==""){
+	$email = JText::_('PLG_DIGICOM_PAY_TEST_AFTER_PAYMENT_CONTACT_NO_ADDRS');
+}
+else{
+	$email = $vars->custom_email;
+}
+$preview_image = $this->params->get('preview_image');
+$buy_image = $this->params->get('buy_image');
+?>
+
+<div class="digicom-payment-form">
+	<form action="<?php echo $vars->url; ?>" name="adminForm" id="adminForm" class="form-validate form-horizontal"  method="post">
+		<div>
+			<?php if($preview_image): ?>
+			<div class="control-group text-center center align-center">
+				<img src="<?php echo $preview_image; ?>" />
+			</div>
+			<?php endif; ?>
+			
+			<div class="control-group">
+				<label for="cardlname" class="control-label"><?php echo JText::_( 'PLG_DIGICOM_PAY_TEST_PAYABLE_AMOUNT' ); ?></label>
+				<div class="controls"><span class="label label-success"><?php echo DigiComSiteHelperPrice::format_price( $vars->amount, $configs->get('currency','USD'), true, $configs ); ?></span></div>
+			</div>
+
+			<div class="form-actions">
+
+				<input type='hidden' name='option' value="com_digicom" />
+				<input type='hidden' name='task' value="cart.processPayment" />
+				<input type='hidden' name='processor' value="test" />
+
+				<input type='hidden' name='order_id' value="<?php echo $vars->order_id;?>" />
+				<input type='hidden' name="total" value="<?php echo sprintf('%02.2f',$vars->amount) ?>" />
+				<input type="hidden" name="user_id" size="10" value="<?php echo $vars->user_id;?>" />
+				<input type='hidden' name='return' value="<?php echo $vars->return;?>" >
+				<input type="hidden" name="plugin_payment_method" value="onsite" />
+
+				<?php if($buy_image) : ?>
+					<input type="image" name="submit"
+						src="<?php echo $this->params->get('buy_image'); ?>" 
+						value="<?php echo JText::_('PLG_DIGICOM_PAY_TEST_SUBMIT'); ?>" 
+						alt="<?php echo $this->params->get('title'); ?>" 
+						class="btn"
+						/>
+				<?php else: ?>
+					<input type="submit" name="submit"
+						class="btn btn-success btn-large btn-lg"
+						value="<?php echo JText::_('PLG_DIGICOM_PAY_TEST_SUBMIT'); ?>" 
+						alt="<?php echo $this->params->get('title'); ?>" />
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php echo JHtml::_('form.token');?>
+	</form>
+</div>
